0a7eb90efb4ec25f0e42cbb687eaf561b6bb8563
ghijj <amolgvaidya@outlook.com>
2023-12-16 22:03:24 -0500

    front end updated to access websocket api. will display all ongoing events, next steps are to make it update real-time/figure out solution for that

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-// API KEY: REDACTED_API_KEY
-
+import WebSocketClient from './components/WebsocketClient';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WebSocketClient />
       </header>
     </div>
   );
