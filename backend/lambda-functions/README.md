77003c2f8f72d3ff2b3b60be4a2e5d5485153228
ghijj <amolgvaidya@outlook.com>
2023-12-17 17:21:26 -0500

    websocket for ongoingEvents works again

--- backend/lambda-functions/README.md
+++ backend/lambda-functions/README.md
@@ -8,6 +8,4 @@ Run `sam build` and then  `sam package     --output-template-file packaged.yaml 
 
 To deploy, run `sam deploy     --template-file packaged.yaml     --stack-name exodb-lambda-stack      --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM     --region us-east-1`
 
-Websocket API url connect: `arn:aws:execute-api:us-east-1:228049799584:gruvv52k29/*/$connect`
-
-Websocket API URL: `wss://gruvv52k29.execute-api.us-east-1.amazonaws.com/dev/`
+Websocket API URL: `wss://aarnwsrtbl.execute-api.us-east-1.amazonaws.com/dev/`
--- backend/lambda-functions/ongoingEvents/ongoingEvents.js
+++ backend/lambda-functions/ongoingEvents/ongoingEvents.js
@@ -1,94 +1,43 @@
-import { S3Client } from "@aws-sdk/client-s3";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, QueryCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
-import { ApiGatewayManagementApiClient, PostToConnectionCommand } from "@aws-sdk/client-apigatewaymanagementapi";
-
-// Initialize the S3 client
-const s3Client = new S3Client({ region: "us-east-1" });
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 // Initialize DynamoDB Client
 const ddbClient = new DynamoDBClient({ region: 'us-east-1' });
 const docClient = DynamoDBDocumentClient.from(ddbClient);
 
-// Initialize the API Gateway Management API client
-const apiGwClient = new ApiGatewayManagementApiClient({
-    region: "us-east-1",
-    endpoint: "https://gruvv52k29.execute-api.us-east-1.amazonaws.com/dev"
-});
-
-const getAllConnections = async () => {
-    const params = {
-        TableName: 'websocket-connections',
-    };
-
-    try {
-        const data = await docClient.send(new ScanCommand(params));
-        return data.Items.map(item => item['connection-id']);
-    } catch (error) {
-        console.error('Error fetching connections:', error);
-        throw error;
-    }
-};
-
-const getOngoingEvents = async () => {
-    const params = {
-        TableName: 'event-data', // Replace with your DynamoDB table name
-        IndexName: 'OngoingEventsIndex', // Specify the GSI name
-        KeyConditionExpression: '#ongoingKey = :ongoingValue',
-        ExpressionAttributeNames: {
-            '#ongoingKey': 'ongoing', // Use the GSI partition key name
-        },
-        ExpressionAttributeValues: {
-            ':ongoingValue': "true", // Note that this is a string
-        },
-    };
-
-    try {
-        const command = new QueryCommand(params);
-        const data = await docClient.send(command);
-        return data.Items;
-    } catch (error) {
-        console.error('Error querying DynamoDB:', error);
-        throw error;
-    }
-};
 export const handler = async (event) => {
-    const endpoint = "wss://gruvv52k29.execute-api.us-east-1.amazonaws.com/dev/";
+    try {
+        console.log('Received WebSocket message:', event);
+        // Query the DynamoDB table for events with ongoing == "true"
+        const params = {
+            TableName: 'event-data', // Replace with your table name
+            IndexName: 'OngoingEventsIndex', // Replace with your GSI name if you have one
+            KeyConditionExpression: 'ongoing = :ongoing',
+            ExpressionAttributeValues: {
+                ':ongoing': 'true'
+            },
+            ProjectionExpression: 'id' // Add this line to specify which attribute(s) to return
+        };
 
-    let message = event.body ? JSON.parse(event.body) : {};
+        const command = new QueryCommand(params);
+        const result = await docClient.send(command);
 
-    if (message.action === 'ongoingEvents') {
-        try {
-            const ongoingEvents = await getOngoingEvents();
-            const eventIds = ongoingEvents.map(event => event.id);
-            console.log('Ongoing Event IDs:', eventIds);
-
-            const postData = JSON.stringify(eventIds);
-
-            // Fetch all active connections
-            const connections = await getAllConnections();
-
-            // Send updates to all connections
-            await Promise.all(connections.map(connectionId => {
-                return apiGwClient.send(new PostToConnectionCommand({
-                    ConnectionId: connectionId,
-                    Data: postData,
-                    Endpoint: endpoint
-                }));
-            }));
-
-            return { statusCode: 200, body: 'Data sent to all clients' };
-        } catch (error) {
-            console.error('Error:', error);
-
-            // Error handling...
-            return {
-                statusCode: 500,
-                body: JSON.stringify({ error: 'Internal Server Error' })
-            };
-        }
-    } else {
-        // Handle other actions...
-        return { statusCode: 400, body: 'Invalid action' };
+        // Extract the event IDs from the query result
+        const eventIds = result.Items.map(item => item.id);
+        console.log(eventIds);
+        // Return the list of event IDs as a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(eventIds)
+        };
+        console.log('Sending WebSocket response:', response);
+        return response;
+    } catch (error) {
+        // Handle any errors gracefully
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal server error' })
+        };
     }
 };
