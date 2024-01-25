5e3870ec4481650d5e4554fce6ffd76ac22ea69b
ghijj <amolgvaidya@outlook.com>
2024-01-25 04:28:08 -0500

    sqs queue created and seperate lambda functions to post urls to queue and consumer lambda function to update database based on urls in queue created

--- backend/lambda-functions/.aws-sam/build/updateFutureEventsDataProcessor/updateFutureEventsDataProcessor.py
+++ backend/lambda-functions/.aws-sam/build/updateFutureEventsDataProcessor/updateFutureEventsDataProcessor.py
@@ -8,9 +8,12 @@ import time
 import boto3
 from botocore.exceptions import ClientError
 
-
-# This lambda function is meant to run periodically to iterate through the most recent events and update DynamoDB accordingly. This is meant to update team sign-ups for events logged in the future
+# This lambda function together with UpdateFutureEventsUrlFinder.py is meant to run periodically to iterate through the most recent events and update DynamoDB accordingly. This is meant to update team sign-ups for events logged in the future. Probably do this once a day?
 # Events will be updated in real-time when they are 'ongoing' and so updating events in the past should not be required. 
+
+# This lambda function will determine which RobotEvents API URLs need to be checked. It will then pass those URLs to updateFutureEventsUrlFinder.py which will individually check each URL one at a time
+# Processing one page of events takes ~10 minutes, meaning processing more than 1 page will go over the Lambda function time limit of 15 minutes
+# When a user accesses an event page, the event data will also be updated then. 
 
 # Unique API Key
 API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiMjkxYWYyMmZjZWZiMDg3ZjM5MjRjZGQwMzI0OGUxY2RhYzExODU3NzliMDI3ODMxNTJkYjJkZmI4NDViZDZhOTk2ODUyZDAzNDExMTcxODUiLCJpYXQiOjE3MDYwMTA5MzguODk2OTU5MSwibmJmIjoxNzA2MDEwOTM4Ljg5Njk2MjksImV4cCI6MjY1Mjc4MjEzOC44ODMxMzEsInN1YiI6IjkwODM3Iiwic2NvcGVzIjpbXX0.RYqSADg9ONvLCGokyA6ukRs3ibpJ7J7mLNOEW2fpXoUQKLKtCM4WjwuYfkXz7AijwRlZfYOvT10YAWxLHsiGd0Q0_LHZraS_qR0PXmfAUhbMoFjk-TY_SNAeYrK_9BHptqmXvxK8nI3upPEbxKwu9sJ8uRtfP05X27gCp7VHwEK4fvTvH4AAuj56zOAJZWHZ1DPRwMea0ZZGMQgM-IKfRy0W4h8kAuDqmkfn0xQR_6zMQtY2s2qX94WdWt5GsOFtj61twjsZ9IVoIvV61ZXixSC21aydI8diT6WtT6pCh8sQouY0b34WkW-paNeSGV8V59sl_fYeEylDU9O7irRS4LbGzp5Qsq2sygRfznHePGFIOtJPLTWi-ocBGn55QUTjWSQrXnkNRsVo2XG3XwOkw4pZxHN_yO2AL13cW8G0J2baX5xTZOj_3c9hZShpb0GEb3s6wP4W0VmyjUbkF2TS_uCkNVyD_Ju__n2XgYLo2L_c5SC-cwujYZvFFeWGXzusCaIE85mFwW11u38HnOwbrt383qlIOdGNV_FCrOEMYvsIPm1laR4zJv0XZR1GosFOAwO6ROKyH-0TTuvUutyu5Ecvp9-C9DuALwjsJCXG5NMpFMzaM0fcx4OPjd50y-ikdxkBbg0MJ5RAaX9z2qJrgtUGKJJ398b2j7LA6jczF54'
@@ -18,6 +21,9 @@ headers = {
     'accept': 'application/json',
     'Authorization': f'Bearer {API_KEY}'
 }
+
+logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
 
 logging.basicConfig(level=logging.INFO)
 logging.basicConfig(level=logging.ERROR)
@@ -32,11 +38,11 @@ def make_request_base(url, headers, initial_delay=5, retries = 5):
         if response.status_code == 200:
             return response.json().get('data', [])
         elif response.status_code == 429:
-            print(f"Rate limit exceeded. Retrying in {initial_delay} seconds...")
+            logging.info(f"Rate limit exceeded when requesting events. Retrying in {initial_delay} seconds...")
             time.sleep(initial_delay)
             initial_delay *= 2
         else:
-            print(f"Request failed with status code: {response.status_code}")
+            logging.info(f"Request for events failed with status code: {response.status_code}")
             break
 
     return []
@@ -49,11 +55,11 @@ def make_request(event_id, url, headers, initial_delay = 5, retries = 5):
         if response.status_code == 200:
             return response.json().get('data', [])
         elif response.status_code == 429:
-            print(f"Rate limit exceeded, Event id: {event_id}. Retrying in {initial_delay} seconds...")
+            logging.info(f"Rate limit exceeded, Event id: {event_id}. Retrying in {initial_delay} seconds...")
             time.sleep(initial_delay)
             initial_delay *= 2
         else:
-            print(f"Request failed with status code: {response.status_code}")
+            logging.info(f"Request failed with status code: {response.status_code}")
             break
 
     return []
@@ -63,7 +69,7 @@ def get_teams(event_id):
     page = 1
     all_teams = []
     while True:
-        api_url = f"https://www.robotevents.com/api/v2/events/{event_id}/teams?page={page}&per_page=250"
+        api_url = f"https://www.robotevents.com/api/v2/events/{event_id}/teams?page={page}&per_page=75"
         teams_data = make_request(event_id, api_url, headers={'Authorization': f'Bearer {API_KEY}'})
         if teams_data:
             team_ids = [team['id'] for team in teams_data]
@@ -77,24 +83,6 @@ def get_teams(event_id):
             break
     return all_teams
 
-def get_last_page(time_to_check, initial_delay=5, retries=5):
-    for _ in range(retries):
-        response = requests.get(f"https://www.robotevents.com/api/v2/events?start={time_to_check}&page=1&per_page=250", headers=headers)
-
-        if response.status_code == 200:
-            return response.json().get('meta', [])['last_page']
-        elif response.status_code == 429:
-            print(f"Rate limit exceeded. Retrying in {initial_delay} seconds...")
-            time.sleep(initial_delay)
-            initial_delay *= 2
-        else:
-            print(time_to_check)
-            print(f"Request failed with status code: {response.status_code}")
-            break
-    return -1
-
-
-
 # DynamoDB functions
 
 def put_item_in_dynamodb(table, item_data):
@@ -103,7 +91,7 @@ def put_item_in_dynamodb(table, item_data):
         response = table.put_item(Item=item_data)
         return response
     except ClientError as e:
-        print(e.response['Error']['Message'])
+        logging.error(e.response['Error']['Message'])
         return None
     
 def convert_values(obj): # Convert floats to decimals and 'ongoing' to a string
@@ -119,47 +107,37 @@ def convert_values(obj): # Convert floats to decimals and 'ongoing' to a string
         return [convert_values(v) for v in obj]
     return obj
 
+
+
 def handler(aws_event, context):
-    current_utc_datetime = datetime.now(pytz.utc)
-    time_to_check = (current_utc_datetime - timedelta(hours=2)).strftime('%Y-%m-%dT%H:%M:%SZ') # get 2 hours prior to the function being called
-    last_page = get_last_page(time_to_check)
+    start_time = time.time()
+    for record in aws_event['Records']:
+        message = json.loads(record['body'])
+        url = message.get('url')
+        data = make_request_base(url, headers)    
 
-    if(last_page == -1): # this indicates a rate limit that isnt resolved through delays, shouldnt happen
-        return
-    combined_data = []
-    for i in range(1, last_page+1): # Iterate through every event that starts after the current time (minus 2 hours)
-        url = f'https://www.robotevents.com/api/v2/events?start={time_to_check}&page={i}&per_page=250' #250 is the max to show per page
-        print(f"Got events page {i}")
-        data = make_request_base(url, headers)
-        combined_data += data
-    
+        logging.info(f"{len(data)} Events to process")
+        count = 1
+        for event in data:
+            event_id = event['id']
+            # Do not add matches, these events are in the future and should have no associated matches
+            # Add teams to events, do this for all events
+            event['teams'] = get_teams(event_id)
 
-    
-    for event in combined_data:
-        event_id = event['id']
-        # Do not add matches, these events are in the future and should have no associated matches
-        # Add teams to events, do this for all events
-        event['teams'] = get_teams(event_id)
+            # Add GSI partition key (used for sorting by start date)
+            event['gsiPartitionKey'] = "ALL_EVENTS"
 
-        # Add GSI partition key (used for sorting by start date)
-        event['gsiPartitionKey'] = "ALL_EVENTS"
+            # Post to DynamoDB. DynamoDB automatically updates the item if it already exists, otherwise will create a new entry
+            response = put_item_in_dynamodb(event_data_table, event)
+            if response:
+                logging.info(f"Event id {event_id} successfully posted to DynamoDB. Event count: {count}")
+            else:
+                logging.info(f"Event id {event_id} failed to post DynamoDB")
+            count += 1
 
-        # Post to DynamoDB. DynamoDB automatically updates the item if it already exists, otherwise will create a new entry
-        response = put_item_in_dynamodb(event_data_table, event)
-        if response:
-            logging.info(f"Event id {event_id} successfully posted to DynamoDB")
-            print(f"Event id {event_id} successfully posted to DynamoDB")
-        else:
-            logging.info(f"Event id {event_id} failed to post DynamoDB")
-            print(f"Event id {event_id} failed to post to DynamoDB")
-
-    print("Process finished")
     logging.info("Process Finished")
+    logging.info(f"Elapsed Time in seconds: {time.time() - start_time}")
     return {
         'statusCode': 200,
         'body': json.dumps('Process Completed Successfully')
     }
-    
-if __name__ == "__main__":
-    main()
-    
