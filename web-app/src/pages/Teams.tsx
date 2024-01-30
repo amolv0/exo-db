b11a72116099ed4c22f5e25a77a1a60579130d5d
Ray <rguo1010@uw.edu>
2024-01-30 15:57:15 -0800

    Basic Site Setup

---
+++ web-app/src/pages/Teams.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Teams: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center">
+        <h1 className="text-white text-2xl font-bold mb-4 my-8">Teams</h1>
+        <p className="text-gray-300">adc diff</p>
+    </div>
+  );
+};
+
+export default Teams;
