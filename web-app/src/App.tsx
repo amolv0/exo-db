7b077e959aa7180c044bfa277f1d96427ff9d467
Ray <rguo1010@uw.edu>
2024-02-06 11:32:25 -0800

    Events / Teams Specific Info
    Teams just ids rn

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -5,13 +5,15 @@ import Teams from './pages/Teams';
 import Events from './pages/Events';
 import Skills from './pages/Skills';
 import EventInfo from './pages/EventInfo';
+import OngoingEvents from './components/OngoingEvents';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-white text-2xl font-bold mb-4 my-8">HOME</h1>
-      <p className="text-gray-300">jg diff</p>
+      <p className="text-gray-300">Ongoing Events</p>
+      <OngoingEvents/>
     </div>
   );
 };
