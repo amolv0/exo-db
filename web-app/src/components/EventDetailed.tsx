352d7d37069f91f910ff0ac2cfeaa8393e32c040
Ray <rguo1010@uw.edu>
2024-02-05 17:23:21 -0800

    Basic Recent Events Display + InDepth info
    Recent events, clickable events that reroute to more in depth information about the event

---
+++ web-app/src/components/EventDetailed.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+interface MyComponentProps {
+  eventId: number;
+}
+
+const MyComponent: React.FC<MyComponentProps> = ({ eventId }) => {
+  const [loading, setLoading] = useState(true);
+  const [eventName, setEventName] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`EXODB_API_GATEWAY_BASE_URL/dev/events/${eventId}`);
+        const eventData = await response.json();
+        // Extract the 'name' property
+        const name = eventData[0].name;
+
+        console.log('Extracted Name:', name);
+
+        setEventName(name); // Set the 'name' to the state
+      } catch (error) {
+        console.error('Error fetching or parsing JSON:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [eventId]);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <p className="text-white text-2m mb-4 my-8">{eventName || 'N/A'}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyComponent;
