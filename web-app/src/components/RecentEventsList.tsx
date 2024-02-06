94009d8e2a83aa223a9a74d8c0d364cd0f51a8b1
Ray <rguo1010@uw.edu>
2024-02-05 17:23:21 -0800

    Basic Recent Events Display + InDepth info
    Recent events, clickable events that reroute to more in depth information about the event

---
+++ web-app/src/components/RecentEventsList.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import EventBasic from './EventBasic';
+
+const App: React.FC = () => {
+  const [eventIds, setEventIds] = useState<number[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch the JSON file for ids of recent 10
+        const response = await fetch('EXODB_API_GATEWAY_BASE_URL/dev/events?numberOfEvents=20');
+        const result = await response.json();
+        setEventIds(result);
+      } catch (error) {
+        console.error('Error fetching event IDs:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        eventIds.map((eventId) => (
+          <EventBasic key={eventId} eventId={eventId} />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default App;
--- web-app/src/pages/Events.tsx
+++ web-app/src/pages/Events.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import WebSocketClient from '../components/WebsocketClient';
+import RecentEventsList from '../components/RecentEventsList';
 
 const Teams: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
-        <h1 className="text-white text-2xl font-bold mb-4 my-8">Events</h1>
-        <WebSocketClient/>
+        <h1 className="text-white text-2xl font-bold mb-4 my-8">Recent Events</h1>
+        <RecentEventsList/>
     </div>
   );
 };
