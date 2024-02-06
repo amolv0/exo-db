665b1c80d66dcd1f208082877460b50628902afe
Ray <rguo1010@uw.edu>
2024-02-05 20:46:00 -0800

    Updated to follow {id : {x}}, fixed N/A issue

--- web-app/src/pages/Skills.tsx
+++ web-app/src/pages/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Teams: React.FC = () => {
+  console.log("wtf");
   return (
     <div className="flex flex-col items-center">
         <h1 className="text-white text-2xl font-bold mb-4 my-8">Skills</h1>
