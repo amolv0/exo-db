17a6157dda0aef3005888500c5d40aaef4b70847
Ray <rguo1010@uw.edu>
2024-02-06 11:32:25 -0800

    Events / Teams Specific Info
    Teams just ids rn

--- web-app/src/pages/Skills.tsx
+++ web-app/src/pages/Skills.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Teams: React.FC = () => {
-  console.log("wtf");
   return (
     <div className="flex flex-col items-center">
         <h1 className="text-white text-2xl font-bold mb-4 my-8">Skills</h1>
