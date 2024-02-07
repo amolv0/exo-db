c4e253ec36853196b8b31f5787fd0566057a7992
Ray <rguo1010@uw.edu>
2024-02-06 16:09:59 -0800

    Post, altered classes to work around it

--- web-app/src/components/RecentEventsList.tsx
+++ web-app/src/components/RecentEventsList.tsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import EventBasic from './EventBasic';
 
 const App: React.FC = () => {
-  const [eventIds, setEventIds] = useState<number[]>([]);
+  const [eventIdsString, setEventIdsString] = useState<string>('');
+  const [eventData, setEventData] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -15,7 +16,9 @@ const App: React.FC = () => {
         // Extract ids and convert them to numbers
         const ids = result.map((event: { id: number }) => event.id);
 
-        setEventIds(ids);
+        // Convert the array of IDs to the desired format
+        const formattedIds = JSON.stringify(ids);
+        setEventIdsString(formattedIds);
       } catch (error) {
         console.error('Error fetching event IDs:', error);
       } finally {
@@ -26,13 +29,39 @@ const App: React.FC = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const fetchEventData = async () => {
+      try {
+        const response = await fetch('EXODB_API_GATEWAY_BASE_URL/dev/events/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: eventIdsString
+        });
+        const data = await response.json();
+        setEventData(data);
+      } catch (error) {
+        console.error('Error fetching or parsing JSON:', error);
+      }
+    };
+
+    if (eventIdsString) {
+      fetchEventData();
+    }
+  }, [eventIdsString]);
+
   return (
     <div>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        eventIds.map((eventId) => (
-          <EventBasic key={eventId} eventId={eventId} />
+        eventData.map((event: any) => (
+          <EventBasic 
+            key = {event.id} 
+            name= {event.name} 
+            eventID = {event.id} 
+          />
         ))
       )}
     </div>
