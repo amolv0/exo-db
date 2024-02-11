8122662089d2d78c13e9c30b8256f029436e5820
Ray <rguo1010@uw.edu>
2024-02-11 15:31:45 -0800

    Updated to fit current api, structure changes + adding teams soon

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -4,16 +4,16 @@ import NavBar from './components/NavBar';
 import Teams from './pages/Teams';
 import Events from './pages/Events';
 import Skills from './pages/Skills';
+import TeamInfo from './pages/TeamInfo';
 import EventInfo from './pages/EventInfo';
 import Division from './pages/Division';
-import OngoingEvents from './components/OngoingEvents';
+import OngoingEvents from './components/EventLists/OngoingEvents';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-white text-2xl font-bold mb-4 my-8">HOME</h1>
-      <p className="text-gray-300">Ongoing Events</p>
       <OngoingEvents/>
     </div>
   );
@@ -26,7 +26,8 @@ const App: React.FC = () => {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/teams" element={<Teams />} />
+          <Route path="/teams" element={<Teams />} />\
+          <Route path="/teams/:teamId" element={<TeamInfo />} />
           <Route path="/events" element={<Events />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/events/:eventId" element={<EventInfo />} />
