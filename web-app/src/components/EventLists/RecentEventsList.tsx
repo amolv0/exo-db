66561c6fd98d814103c9590b574db841b031476d
Ray <rguo1010@uw.edu>
2024-02-12 16:51:39 -0800

    Teams display, general display updates

--- web-app/src/components/EventLists/RecentEventsList.tsx
+++ web-app/src/components/EventLists/RecentEventsList.tsx
@@ -3,13 +3,12 @@ import CreateList from './Helpers/CreateList';
 
 const App: React.FC = () => {
   const [eventIdsString, setEventIdsString] = useState<string>('');
-  const [eventData, setEventData] = useState<any[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         // Fetch the JSON file for ids of recent 10
-        const response = await fetch('EXODB_API_GATEWAY_BASE_URL/dev/events?numberOfEvents=20?program=VRC');
+        const response = await fetch('EXODB_API_GATEWAY_BASE_URL/dev/events?numberOfEvents=50&program=VRC');
         const result = await response.json();
         // Convert the array of IDs to the desired format
         const formattedIds = JSON.stringify(result);
@@ -21,30 +20,8 @@ const App: React.FC = () => {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    const fetchEventData = async () => {
-      try {
-        const response = await fetch('EXODB_API_GATEWAY_BASE_URL/dev/events/', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: eventIdsString
-        });
-        const data = await response.json();
-        setEventData(data);
-      } catch (error) {
-        console.error('Error fetching or parsing JSON:', error);
-      }
-    };
-
-    if (eventIdsString) {
-      fetchEventData();
-    }
-  }, [eventIdsString]);
-
   return (
-    <CreateList eventData={eventData}/>
+    <CreateList eventIdsString={eventIdsString}/>
   );
 };
 
