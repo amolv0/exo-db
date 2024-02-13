c72df0afa81058d5527a38950d336a6dd7a485b2
Ray <rguo1010@uw.edu>
2024-02-12 16:51:39 -0800

    Teams display, general display updates

--- web-app/src/pages/Events.tsx
+++ web-app/src/pages/Events.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import RecentEventsList from '../components/EventLists/RecentEventsList';
 
 const Teams: React.FC = () => {
-  console.log("wtf");
   return (
     <div className="flex flex-col items-center">
         <h1 className="text-white text-2xl font-bold mb-4 my-8">Recent Events</h1>
