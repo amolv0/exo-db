65d0cf7c3adb54d7ea640e82a4de3dbffe98cbdf
Ray <rguo1010@uw.edu>
2024-02-12 16:51:39 -0800

    Teams display, general display updates

--- web-app/src/components/NavBar.tsx
+++ web-app/src/components/NavBar.tsx
@@ -3,33 +3,33 @@ import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container ml-4 lg:ml-40 xl:ml-60 2xl:ml-80">
-        <div className="flex items-center">
-          {/* Logo or Brand */}
-          <div className="text-white text-xl font-bold mr-10">
-            <Link to="/">Vex Stats</Link>
-          </div>
+    <nav className="bg-gray-800 p-4 text-xl">
+      <div className="container mx-auto flex justify-between items-center">
 
-          {/* Navigation Links */}
-          <ul className="flex space-x-4">
-            <li className="text-white">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="text-white">
-              <Link to="/teams">Teams</Link>
-            </li>
-            <li className="text-white">
-              <Link to="/events">Events</Link>
-            </li>
-            <li className="text-white">
-              <Link to="/skills">Skills</Link>
-            </li>
-          </ul>
+        <ul className="flex space-x-4">
+          <li>
+            <div className="text-white text-xl font-bold lg:mr-36">
+              <Link to="/">Vex Stats</Link>
+            </div>
+          </li>
+          <li className="text-white">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="text-white">
+            <Link to="/teams">Teams</Link>
+          </li>
+          <li className="text-white">
+            <Link to="/events">Events</Link>
+          </li>
+          <li className="text-white">
+            <Link to="/skills">Skills</Link>
+          </li>
+        </ul>
 
-          <div className="text-white text-xl flex mx-auto">
-            <h1>Search Placeholder</h1>
-          </div>
+        {/* Search */}
+        <div className="text-white text-xl">
+          {/* Adjust the style of the input field as needed */}
+          <input type="text" placeholder="Search" className="px-2 py-1 rounded-md bg-gray-700 text-white focus:outline-none" />
         </div>
       </div>
     </nav>
