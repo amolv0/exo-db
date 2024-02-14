15f449dbadddcc7c8ae85adcb1ff13f4a38a9f2f
Ray <rguo1010@uw.edu>
2024-02-14 00:21:23 -0800

    Teams display for events

--- web-app/src/components/EventInfo/EventLocation.tsx
+++ web-app/src/components/EventInfo/EventLocation.tsx
@@ -33,31 +33,32 @@ interface JSONComponentProps {
 
 const JSONComponent: React.FC<JSONComponentProps> = ({ location, season, name, program }) => {
   return (
-    <div className="max-w-md mx-auto bg-black text-white p-4 rounded-lg shadow-md">
+    <div className="mx-auto bg-gray-700 text-white p-4 rounded-lg shadow-md mt-4">
       <h2 className="text-xl font-semibold mb-4">Event Data</h2>
-        {location && (
-          <div className="mb-6">
-            <h3 className="text-lg font-medium mb-2">Location</h3>
-            <p>Venue: {location.venue || 'N/A'}</p>
-            <p>Country: {location.country || 'N/A'}</p>
-            <p>City: {location.city || 'N/A'}</p>
-            <p>Address: {location.address_1 || 'N/A'}</p>
-            <p>Region: {location.region || 'N/A'}</p>
-          </div>
-        )}
-        {season && (
-          <div className="mb-6">
-            <h3 className="text-lg font-medium mb-2">Season</h3>
-            <p>{season.name}</p>
-          </div>
-        )}
-        {program && (
-          <div className="mb-6">
-            <h3 className="text-lg font-medium mb-2">Program</h3>
-            <p>{program || 'N/A'}</p>
-          </div>
-        )}
+      {location && (
+        <div className="mb-6">
+          <h3 className="text-lg font-medium mb-2">Location</h3>
+          <p>Venue: {location.venue || 'N/A'}</p>
+          <p>Country: {location.country || 'N/A'}</p>
+          <p>City: {location.city || 'N/A'}</p>
+          <p>Address: {location.address_1 || 'N/A'}</p>
+          <p>Region: {location.region || 'N/A'}</p>
+        </div>
+      )}
+      {season && (
+        <div className="mb-6">
+          <h3 className="text-lg font-medium mb-2">Season</h3>
+          <p>{season.name || 'N/A'}</p>
+        </div>
+      )}
+      {program && (
+        <div className="mb-6">
+          <h3 className="text-lg font-medium mb-2">Program</h3>
+          <p>{program || 'N/A'}</p>
+        </div>
+      )}
     </div>
+
   );
 };
 
