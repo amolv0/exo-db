2199f15afa6489111ba134a382decddfade793e3
ghijj <amolgvaidya@outlook.com>
2024-02-14 00:12:48 -0500

    match get/post implemented, updates to future event processing to account for awards, rankings, skills as well as to only write updates rather than the whole item every pass.

--- backend/lambda-functions/otherFuncs/updateFutureEvents/updateFutureEventsDataProcessor/updateFutureEventsDataProcessor.py
+++ backend/lambda-functions/otherFuncs/updateFutureEvents/updateFutureEventsDataProcessor/updateFutureEventsDataProcessor.py
@@ -65,12 +65,15 @@ def make_request(event_id, url, headers, initial_delay = 5, retries = 5):
 def get_teams(event_id):
     page = 1
     all_teams = []
+    all_team_numbers = []
     while True:
         api_url = f"https://www.robotevents.com/api/v2/events/{event_id}/teams?page={page}&per_page=75"
         teams_data = make_request(event_id, api_url, headers={'Authorization': f'Bearer {API_KEY}'})
         if teams_data:
             team_ids = [team['id'] for team in teams_data]
+            team_numbers = [team['number'] for team in teams_data]  # Collecting team numbers
             all_teams.extend(team_ids)
+            all_team_numbers.extend(team_numbers)  # Adding team numbers to the list
             last_page = teams_data[0].get('meta', {}).get('last_page', 0)
             if page >= last_page:
                 break
@@ -78,18 +81,26 @@ def get_teams(event_id):
                 page += 1
         else:
             break
-    return all_teams
+    return all_teams, all_team_numbers  # Returning both team IDs and team numbers
+
+def get_awards(event_id):
+    page = 1
+    all_awards = []
+    while True:
+        api_url = f"https://www.robotevents.com/api/v2/events/{event_id}/awards?page={page}&per_page=75"
+        awards_data = make_request(event_id, api_url, headers={'Authorization': f'Bearer {API_KEY}'})
+        if awards_data:
+            all_awards.extend(awards_data)  # Adding all awards from the current page to the list
+            last_page = awards_data[0].get('meta', {}).get('last_page', 0)
+            if page >= last_page:
+                break
+            else:
+                page += 1
+        else:
+            break
+    return all_awards  # Returning the compiled list of awards
 
 # DynamoDB functions
-
-def put_item_in_dynamodb(table, item_data):
-    item_data = convert_values(item_data)
-    try:
-        response = table.put_item(Item=item_data)
-        return response
-    except ClientError as e:
-        logging.error(e.response['Error']['Message'])
-        return None
     
 def convert_values(obj): # Convert floats to decimals and 'ongoing' to a string
     if isinstance(obj, float):
@@ -106,44 +117,96 @@ def convert_values(obj): # Convert floats to decimals and 'ongoing' to a string
 
 
 
+def get_item_from_dynamodb(table, event_id):
+    try:
+        response = table.get_item(Key={'id': event_id})
+        return response.get('Item')
+    except ClientError as e:
+        logging.error(e.response['Error']['Message'])
+        return None
+
+def update_item_in_dynamodb(table, event_id, update_expression, expression_attribute_values):
+    try:
+        response = table.update_item(
+            Key={'id': event_id},
+            UpdateExpression=update_expression,
+            ExpressionAttributeValues=expression_attribute_values,
+            ReturnValues="UPDATED_NEW"
+        )
+        return response
+    except ClientError as e:
+        logging.error(e.response['Error']['Message'])
+        return None
+
 def handler(aws_event, context):
     start_time = time.time()
     for record in aws_event['Records']:
         message = json.loads(record['body'])
         url = message.get('url')
-        data = make_request_base(url, headers)    
+        data = make_request_base(url, headers)
 
         logging.info(f"{len(data)} Events to process")
-        count = 1
         for event in data:
             event_id = event['id']
 
-            # Determine the 'region' attribute based on 'location'
-            if 'location' in event:
-                if 'region' in event['location'] and event['location']['region']:
-                    event['region'] = event['location']['region']
-                elif 'country' in event['location'] and event['location']['country']:
-                    event['region'] = event['location']['country']
-                else:
-                    event['region'] = 'Unknown'
+            # Fetch the current event data from DynamoDB
+            current_event_data = get_item_from_dynamodb(event_data_table, event_id)
 
-            if 'program' in event and 'code' in event['program']:  # Flatten program to just the string representation of the code
-                program_code = event['program']['code']
-                event['program'] = program_code
+            # Initialize update flags and update parameters
+            update_expression = "SET "
+            expression_attribute_values = {}
+            updated = False
             
-            # Add teams to events, do this for all events
-            event['teams'] = get_teams(event_id)
+            # Check and update 'teams' and 'team_numbers'
+            teams, team_numbers = get_teams(event_id)
+            if current_event_data is None or 'teams' not in current_event_data or current_event_data['teams'] != teams:
+                update_expression += "teams = :teams, "
+                expression_attribute_values[':teams'] = teams
+                updated = True
 
-            # Add GSI partition key (used for sorting by start date)
-            event['gsiPartitionKey'] = "ALL_EVENTS"
+            if current_event_data is None or 'team_numbers' not in current_event_data or current_event_data['team_numbers'] != team_numbers:
+                update_expression += "team_numbers = :teamNumbers, "
+                expression_attribute_values[':teamNumbers'] = team_numbers
+                updated = True
 
-            # Post to DynamoDB. DynamoDB automatically updates the item if it already exists, otherwise will create a new entry
-            response = put_item_in_dynamodb(event_data_table, event)
-            if response:
-                logging.info(f"Event id {event_id} successfully posted to DynamoDB. Event count: {count}")
+            # Check and update 'awards'
+            awards = get_awards(event_id)
+            if current_event_data is None or 'awards' not in current_event_data or current_event_data['awards'] != awards:
+                update_expression += "awards = :awards, "
+                expression_attribute_values[':awards'] = awards
+                updated = True
+
+            # Check and update 'divisions' and ensure each division has an empty 'rankings' list
+            divisions_changed = False
+            if 'divisions' in event:
+                for division in event['divisions']:
+                    if 'rankings' not in division:
+                        division['rankings'] = []  # Ensure each division has an empty 'rankings' list
+                        divisions_changed = True  # Mark divisions as changed if any division didn't have 'rankings'
+
+                # Check if divisions have changed compared to what's stored in DynamoDB
+                if current_event_data is None or 'divisions' not in current_event_data or current_event_data['divisions'] != event['divisions']:
+                    divisions_changed = True
+
+            if divisions_changed:
+                update_expression += "divisions = :divisions, "
+                expression_attribute_values[':divisions'] = event['divisions']
+                updated = True
+
+            # Finalize update expression and perform update if necessary
+            if updated:
+                update_expression = update_expression.rstrip(", ")  # Remove the trailing comma and space
+                
+                # Apply convert_values to each value in expression_attribute_values
+                expression_attribute_values = {k: convert_values(v) for k, v in expression_attribute_values.items()}
+
+                response = update_item_in_dynamodb(event_data_table, event_id, update_expression, expression_attribute_values)
+                if response:
+                    logging.info(f"Event id {event_id} successfully updated in DynamoDB.")
+                else:
+                    logging.info(f"Failed to update event id {event_id} in DynamoDB.")
             else:
-                logging.info(f"Event id {event_id} failed to post DynamoDB")
-            count += 1
+                logging.info(f"No updates required for event id {event_id}.")
 
     logging.info("Process Finished")
     logging.info(f"Elapsed Time in seconds: {time.time() - start_time}")
