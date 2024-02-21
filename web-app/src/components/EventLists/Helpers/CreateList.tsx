097f34aae29f508cb2a4710ae869922bd9859299
Ray <rguo1010@uw.edu>
2024-02-21 12:56:38 -0800

    Changes so it runs

--- web-app/src/components/EventLists/Helpers/CreateList.tsx
+++ web-app/src/components/EventLists/Helpers/CreateList.tsx
@@ -6,7 +6,8 @@ interface EventListDisplayProps {
 }
 
 const EventListDisplay: React.FC<EventListDisplayProps> = ({ eventIdsString }) => {
-  const [eventData, setEventData] = useState<any[]>([]);
+
+  const [maps, setMaps] = useState<any[]>([]);
   useEffect(() => {
     const fetchEventData = async () => {
       try {
@@ -18,7 +19,7 @@ const EventListDisplay: React.FC<EventListDisplayProps> = ({ eventIdsString }) =
           body: eventIdsString
         });
         const data = await response.json();
-        setEventData(data);
+        setMaps(data);
       } catch (error) {
         console.error('Error fetching or parsing JSON:', error);
       }
@@ -31,35 +32,35 @@ const EventListDisplay: React.FC<EventListDisplayProps> = ({ eventIdsString }) =
 
   return (
     <div>
-  {/* Header for larger screens */}
-  <div className="text-xl font-bold text-gray-50 flex flex-wrap" style={{ width: '100%' }}>
-    <div className= "hidden md:block" style={{ flex: 1 }}>Program</div>
-    <div style={{ flex: 3 }}>Event</div>
-    <div style={{ flex: 2 }}>Location</div>
-    <div style={{ flex: 2 }}>Date</div>
-  </div>
+      {/* Header for larger screens */}
+      <div className="text-xl font-bold text-gray-50 flex flex-wrap" style={{ width: '100%' }}>
+        <div className= "hidden md:block" style={{ flex: 1 }}>Program</div>
+        <div style={{ flex: 3 }}>Event</div>
+        <div style={{ flex: 2 }}>Location</div>
+        <div style={{ flex: 2 }}>Date</div>
+      </div>
 
-  {/* Header for smaller screens (invisible) */}
-  <div className="text-xl font-bold text-gray-50 invisible flex flex-wrap" style={{ width: '100%' }}>
-    <div className = "hidden md:block">Thisissomeweird</div>
-    <div className = "hidden md:block">solutionbutitworkthisijustfiller bufferheheIcantwithmyself</div>
-    <div className = "hidden md:block">soisitrightorwronglmao</div>
-    <div className = "hidden md:block">xdxdxdxdxd</div>
-  </div>
+      {/* Header for smaller screens (invisible) */}
+      <div className="text-xl font-bold text-gray-50 invisible flex flex-wrap" style={{ width: '100%' }}>
+        <div className = "hidden md:block">Thisissomeweird</div>
+        <div className = "hidden md:block">solutionbutitworkthisijustfiller bufferheheIcantwithmyself</div>
+        <div className = "hidden md:block">soisitrightorwronglmao</div>
+        <div className = "hidden md:block">xdxdxdxdxd</div>
+      </div>
 
-  <div>
-    {eventData.map((event) => (
-      <EventBasic 
-        key={event.id} 
-        name={event.name} 
-        eventID={event.id} 
-        program={event.program}
-        location={event.location}
-        start={event.start}
-        end={event.end}
-      />
-    ))}
-  </div>
+      <div>
+      {maps.map((event) => (
+            <EventBasic 
+              key={event.id} 
+              name={event.name} 
+              eventID={event.id} 
+              prog={event.program.code || event.program}
+              location={event.location}
+              start={event.start}
+              end={event.end}
+            />
+          ))}
+      </div>
 </div>
 
   );
