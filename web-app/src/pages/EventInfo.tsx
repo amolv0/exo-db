bfe94f0319c752958fa96c2f0c712f13ba3c74f4
ghijj <amolgvaidya@outlook.com>
2024-02-25 21:51:03 -0500

    ui updates

--- web-app/src/pages/EventInfo.tsx
+++ web-app/src/pages/EventInfo.tsx
@@ -5,6 +5,7 @@ import TeamsList from '../components/EventInfo/TeamsList';
 import MatchesList from '../components/EventInfo/MatchesList';
 import EventRankings from '../components/EventInfo/EventRankings';
 import EventSkills from '../components/EventInfo/EventSkills';
+import { Box, Typography, Button, ButtonGroup, CircularProgress } from '@mui/material';
 
 const EventInfo: React.FC = () => {
   const { eventId } = useParams<{ eventId: string }>();
@@ -33,50 +34,53 @@ const EventInfo: React.FC = () => {
   }, [eventId]);
 
   return (
-    <div>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', my: 4, bgcolor: 'lightgrey', width: '100%' }}>
       {loading ? (
-        <p className="text-gray-200 text-2xl mb-4 mt-8 text-center">Loading...</p>
+        <CircularProgress color="inherit" />
       ) : (
-        <div>
-          {eventData && eventData.length > 0 ? (
-            <React.Fragment>
-              <h2 className="text-gray-200 text-2xl mb-4 mt-8 text-center">Event Details for {eventData[0].name}</h2>
-              
-              <div className="max-w-5xl mx-auto bg-gray-800 text-gray-200 p-4 rounded-lg shadow-md">
-                <table className="w-full table-auto border-collapse">
-                  <thead>
-                    <tr className="bg-gray-600">
-                      <th className="py-2 px-4 cursor-pointer" onClick={() => handleHeaderClick('EventInfo')}>Event Info</th>
-                      <th className="py-2 px-4 cursor-pointer" onClick={() => handleHeaderClick('TeamsList')}>Teams List</th>
-                      <th className="py-2 px-4 cursor-pointer" onClick={() => handleHeaderClick('MatchesList')}>Matches</th>
-                      <th className="py-2 px-4 cursor-pointer" onClick={() => handleHeaderClick('Rankings')}>Rankings</th>
-                      <th className="py-2 px-4 cursor-pointer" onClick={() => handleHeaderClick('Element4')}>Elims</th>
-                      <th className="py-2 px-4 cursor-pointer" onClick={() => handleHeaderClick('Skills')}>Skills</th>
-                    </tr>
-                  </thead>
-                </table>
-                {activeElement === 'EventInfo' && <EventLocation 
-                    location={eventData[0].location}
-                    season={eventData[0].season}
-                    program={eventData[0].program.code}
-                    awards={eventData[0].awards}
-                />}
-                {activeElement === 'TeamsList' && <TeamsList teams={eventData[0].teams}/>}
-                {activeElement === 'MatchesList' && <MatchesList division={eventData[0].divisions[0]}/>}
-                {activeElement === 'Rankings' && <EventRankings rankings={eventData[0].divisions[0].rankings} />}
-                {activeElement === 'Element4' && <Element4 />}
-                {activeElement === 'Skills' && <EventSkills skills = {eventData[0].skills} />}
-              </div>
-            </React.Fragment>
-          ) : (
-            <p className="text-gray-200 text-2m mb-4 my-8">Match not found for Event ID {eventId}</p>
-          )}
-        </div>
+        eventData && eventData.length > 0 ? (
+          <Box sx={{ width: '100%', maxWidth: '800px', bgcolor: 'white', color: 'text.primary', p: 2, borderRadius: 2, boxShadow: 3 }}>
+            <Typography variant="h4" color="textPrimary" align="center" my={4}>
+              Event Details for {eventData[0].name}
+            </Typography>
+            
+            <Box sx={{ display: 'flex', justifyContent: 'center', bgcolor: 'darkgrey', p: 1, borderRadius: '4px' }}>
+              <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                {['Event Info', 'Teams List', 'Matches', 'Rankings', 'Elims', 'Skills'].map((element, index) => (
+                  <Button
+                    key={index}
+                    onClick={() => handleHeaderClick(element.replace(/\s+/g, ''))}
+                    sx={{
+                      bgcolor: 'darkgrey',
+                      '&:hover': { bgcolor: 'grey.700' },
+                      color: 'white',
+                      '&:focus': { // Targeting the focus state
+                        outline: 'none'
+                      }
+                    }}
+                  >
+                    {element}
+                  </Button>
+                ))}
+              </ButtonGroup>
+            </Box>
+            {activeElement === 'EventInfo' && <EventLocation location={eventData[0].location} season={eventData[0].season} program={eventData[0].program.code} awards={eventData[0].awards} />}
+            {activeElement === 'TeamsList' && <TeamsList teams={eventData[0].teams} />}
+            {activeElement === 'MatchesList' && <MatchesList division={eventData[0].divisions[0]} />}
+            {activeElement === 'Rankings' && <EventRankings rankings={eventData[0].divisions[0].rankings} />}
+            {activeElement === 'Element4' && <Element4 />}
+            {activeElement === 'Skills' && <EventSkills skills={eventData[0].skills} />}
+          </Box>
+        ) : (
+          <Typography variant="h6" color="textSecondary" align="center">
+            Match not found for Event ID {eventId}
+          </Typography>
+        )
       )}
-    </div>
+    </Box>
   );
 };
 
-const Element4: React.FC = () => <div className="mt-4 p-4 bg-gray-100 rounded-lg">Element 5 Content</div>;
+const Element4: React.FC = () => <Box sx={{ mt: 2, p: 2, bgcolor: 'grey.100', borderRadius: 2 }}>Element 5 Content</Box>;
 
 export default EventInfo;
