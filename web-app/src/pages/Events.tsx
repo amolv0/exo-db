84d6c1f8ccedbeb0b454887ece09b52cf69a5f42
ghijj <amolgvaidya@outlook.com>
2024-02-29 04:05:16 -0500

    Home page now does upcoming events sorted properly

--- web-app/src/pages/Events.tsx
+++ web-app/src/pages/Events.tsx
@@ -4,7 +4,7 @@ import RecentEventsList from '../components/EventLists/RecentEventsList';
 const Teams: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
-        <h1 className="text-white text-2xl font-bold mb-4 my-8">Recent Events</h1>
+        <h1 className="text-white text-2xl font-bold mb-4 my-8">Upcoming Events</h1>
         <RecentEventsList/>
     </div>
   );
