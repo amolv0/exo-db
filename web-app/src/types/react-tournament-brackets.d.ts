353be6c8a1fa820bbf096de7b11e49b62f80618a
ghijj <amolgvaidya@outlook.com>
2024-02-28 22:28:15 -0500

    fix pagination for skills ranking GET

---
+++ web-app/src/types/react-tournament-brackets.d.ts
@@ -0,0 +1,6 @@
+declare module '@g-loot/react-tournament-brackets' {
+    export const SingleEliminationBracket: any;
+    export const DoubleEliminationBracket: any;
+    export const Match: any;
+    export const SVGViewer: any;
+  }
