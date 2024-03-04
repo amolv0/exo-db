c55313f01745e13bee02de375140be8077fe7e98
ghijj <amolgvaidya@outlook.com>
2024-03-04 06:33:07 -0500

    last page scrips and slight adjustments to elo/skillsranking/ts get apis

--- database/calculate_skills_rankings.py
+++ database/calculate_skills_rankings.py
@@ -2,19 +2,17 @@ import boto3
 from boto3.dynamodb.conditions import Attr, Key
 from botocore.exceptions import ClientError
 from decimal import Decimal
-import logging
 
 dynamodb = boto3.resource('dynamodb')
 skills_table = dynamodb.Table('skills-ranking-data')
 team_table = dynamodb.Table('team-data')
 
-logging = logging.getLogger()
-logging.setLevel("INFO")
+SEASON = 175
 
 def query_skills_scores(season, skill_type):
     items = []
     last_evaluated_key = None
-    logging.info("Scanning")
+    print("Scanning")
     while True:
         if last_evaluated_key:
             response = skills_table.query(
@@ -39,29 +37,30 @@ def query_skills_scores(season, skill_type):
     return items
 
 def compute_rankings(items, skill_type):
-    # Sort items by score and programming_component as a tiebreaker
     if skill_type == 'robot': items.sort(key=lambda x: (-x['score'], -x.get('programming_component', 0)))
-
     global_rankings = {}
     regional_rankings = {}
     seen_teams = set()
-
     for item in items:
+        
         team_id = item['team_id']
         region = item['region']
         score = item['score']
         if team_id not in seen_teams:
+            
             seen_teams.add(team_id)
             # Update global ranking
             if team_id not in global_rankings:
                 global_rankings[team_id] = score
-            # Update regional ranking
+
+            # # Update regional ranking
             if region not in regional_rankings:
                 regional_rankings[region] = {}
             if team_id not in regional_rankings[region]:
                 regional_rankings[region][team_id] = score
 
     return global_rankings, regional_rankings
+
 
 def update_skills_rankings(team_id, season, skill_type, rank):
     try:
@@ -74,7 +73,7 @@ def update_skills_rankings(team_id, season, skill_type, rank):
             ExpressionAttributeValues={':rank': Decimal(str(rank))},
             ReturnValues="UPDATED_NEW"
         )
-        # logging.info(f"Updated team skills rank for team {team_id} for type: {skill_type}, season {season}")
+        print(f"Updated team skills rank for team {team_id} for type: {skill_type}, season {season}")
     except team_table.meta.client.exceptions.ConditionalCheckFailedException:
         # If elo_rankings does not exist, initialize it and then set the season's ELO rating
         response = team_table.update_item(
@@ -83,7 +82,7 @@ def update_skills_rankings(team_id, season, skill_type, rank):
             ExpressionAttributeValues={':empty_map': {str(season): {str(skill_type) : Decimal(str(rank))} }},
             ReturnValues="UPDATED_NEW"
         )
-        # logging.info(f"Created team skills rank object for team {team_id} for type: {skill_type}, season {season}")
+        print(f"Created team skills rank object for team {team_id} for type: {skill_type}, season {season}")
     except ClientError as e:
         if e.response['Error']['Code'] == 'ValidationException':
             response = team_table.update_item(
@@ -95,10 +94,10 @@ def update_skills_rankings(team_id, season, skill_type, rank):
                 },
                 ReturnValues="UPDATED_NEW"
             )
-            # logging.info(f"Updated team skills rank for team {team_id} for type: {skill_type}, adding a new season {season}")
-        else: logging.error(f"Error updating team skills rank for team exception 1: {team_id}: {e}")
+            print(f"Updated team skills rank for team {team_id} for type: {skill_type}, adding a new season {season}")
+        else: print(f"Error updating team skills rank for team exception 1: {team_id}: {e}")
     except Exception as e:
-        logging.error(f"Error updating team skills rank for team: {team_id}: {e}")
+        print(f"Error updating team skills rank for team: {team_id}: {e}")
 
 def update_skills_regional_rankings(team_id, season, skill_type, rank):
     try:
@@ -112,7 +111,7 @@ def update_skills_regional_rankings(team_id, season, skill_type, rank):
             ExpressionAttributeValues={':rank': Decimal(str(rank))},
             ReturnValues="UPDATED_NEW"
         )
-        # logging.info(f"Updated team skills regional rank for team {team_id} for type: {skill_type}, season {season}")
+        print(f"Updated team skills regional rank for team {team_id} for type: {skill_type}, season {season}")
     except team_table.meta.client.exceptions.ConditionalCheckFailedException:
         
         # If elo_rankings does not exist, initialize it and then set the season's ELO rating
@@ -122,7 +121,7 @@ def update_skills_regional_rankings(team_id, season, skill_type, rank):
             ExpressionAttributeValues={':empty_map': {str(season): {str(skill_type) : Decimal(str(rank))} }},
             ReturnValues="UPDATED_NEW"
         )
-        # logging.info(f"Created team skills regional rank for team {team_id} for type: {skill_type}, season {season}")
+        print(f"Created team skills regional rank for team {team_id} for type: {skill_type}, season {season}")
     except ClientError as e:
         if e.response['Error']['Code'] == 'ValidationException':
             response = team_table.update_item(
@@ -134,10 +133,10 @@ def update_skills_regional_rankings(team_id, season, skill_type, rank):
                 },
                 ReturnValues="UPDATED_NEW"
             )
-            # logging.info(f"Updated team skills regional rank for team {team_id} for type: {skill_type}, adding a new season {season}")
-        else: logging.error(f"Error updating team skills rank for team exception 1: {team_id}: {e}")
+            print(f"Updated team skills regional rank for team {team_id} for type: {skill_type}, adding a new season {season}")
+        else: print(f"Error updating team skills rank for team exception 1: {team_id}: {e}")
     except Exception as e:
-        logging.error(f"Error updating team skills regional rank for team: {team_id}: {e}")
+        print(f"Error updating team skills regional rank for team: {team_id}: {e}")
 
 
 def delete_skills_rankings_for_and_teams(team_id):
@@ -147,13 +146,13 @@ def delete_skills_rankings_for_and_teams(team_id):
             UpdateExpression="REMOVE skills_rankings",
             ReturnValues="UPDATED_NEW"
         )
-        # logging.info(f"Removed skills_rankings from team {team_id}")
+        print(f"Removed skills_rankings from team {team_id}")
     except Exception as e:
-        logging.error(f"Error removing skills_rankings for team {team_id}: {e}")
+        print(f"Error removing skills_rankings for team {team_id}: {e}")
 
 def update_team_rankings(season):
-    skill_types = ['robot', 'programming', 'driver']
-
+    # skill_types = ['robot', 'programming', 'driver']
+    skill_types = ['robot']
     for skill_type in skill_types:
         items = query_skills_scores(season, skill_type)
         global_rankings, regional_rankings = compute_rankings(items, skill_type)
@@ -165,8 +164,10 @@ def update_team_rankings(season):
         for region, rankings in regional_rankings.items():
             for rank, (team_id, score) in enumerate(rankings.items(), start=1):
                 update_skills_regional_rankings(team_id, season, skill_type, rank)
+                
 
 seasons = [173, 175, 154, 156, 139, 140, 130, 131, 125, 126, 119, 120, 115, 116, 110, 111, 102, 103]
+# seasons = 173
 for season in seasons:
     update_team_rankings(season)
     
