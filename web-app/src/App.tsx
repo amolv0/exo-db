62e5d69b86f89273f5f386d628d21e75785bd74e
Ray <rguo1010@uw.edu>
2024-03-04 20:13:54 -0800

    Lots of UI changes, query additions, awaiting region enum
    cook

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
-import Teams from './pages/Teams';
 import Events from './pages/Events';
 import Skills from './pages/Skills';
 import TeamInfo from './pages/TeamInfo';
@@ -14,7 +13,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-white text-2xl font-bold mb-4 my-8">HOME</h1>
+      <h1 className="text-black text-3xl mb-4 my-8">Ongoing Events</h1>
         <OngoingEvents/>
     </div>
   );
@@ -27,7 +26,6 @@ const App: React.FC = () => {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/teams" element={<Teams />} />\
           <Route path="/teams/:teamId" element={<TeamInfo />} />
           <Route path="/events" element={<Events />} />
           <Route path="/skills" element={<Skills />} />
