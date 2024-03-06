db65caae25010784447aaef65d0090ac21ee51a4
Ray <rguo1010@uw.edu>
2024-03-05 18:51:53 -0800

    Reworking team profile display
    in progress

--- web-app/src/components/EventLists/Helpers/EventBasic.tsx
+++ web-app/src/components/EventLists/Helpers/EventBasic.tsx
@@ -27,17 +27,17 @@ const JSONComponent: React.FC<JSONComponentProps> = ({ name, eventID, prog, loca
     <div className = "flex flex-wrap">
       {name && (
         <div style={{ display: 'flex', width: '100%' }}>
-          <div className = "text-stone-400 hidden md:block" style={{ flex: 1 }}>{prog}</div>
-          <div className = "text-orange-100" style={{ flex: 3 }}>
+          <div className = "text-black-500 hidden md:block" style={{ flex: 1 }}>{prog}</div>
+          <div className = "text-orange-300" style={{ flex: 3 }}>
             <Link to={`/events/${eventID}`}>
               {name || 'N/A'}
             </Link>
           </div>
-          <div className="text-stone-400" style={{ flex: 2 }}>
+          <div className="text-black-500" style={{ flex: 2 }}>
             {location?.city && (<div>{location.city}, {location.country}</div>)}
             {!location?.city && location?.country}
           </div>
-          <div className = "text-orange-100" style={{ flex: 2 }}>
+          <div className = "text-orange-300" style={{ flex: 2 }}>
             {start && (start.substring(0, 10) === end?.substring(0, 10) ? start.substring(0, 10) : start.substring(0, 10) + ' - ' + end?.substring(0, 10))}
           </div>
         </div>
