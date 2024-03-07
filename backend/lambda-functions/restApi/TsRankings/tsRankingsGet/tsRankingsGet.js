43814b5d203188a27982f92549b143121fe0eebd
ghijj <amolgvaidya@outlook.com>
2024-03-07 12:56:43 -0500

    EventsGet now works with levels + minor fixes for working with countries for rankings

--- backend/lambda-functions/restApi/TsRankings/tsRankingsGet/tsRankingsGet.js
+++ backend/lambda-functions/restApi/TsRankings/tsRankingsGet/tsRankingsGet.js
@@ -75,6 +75,7 @@ const fetchPage = async (season, desiredPage = 1, regions) => {
     console.log("Length:", results.length);
     return results;
 };
+// We should consider making this an external function that is importable
 const determineRegions = async (input) => {
     const regions = {
         // Detailed regions for specific countries
