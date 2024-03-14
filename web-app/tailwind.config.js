6ff80939afea189ae826db56a7e070cb60dbaa11
Ray <rguo1010@uw.edu>
2024-03-14 12:01:01 -0700

    Integrating UI
    Working on table

--- web-app/tailwind.config.js
+++ web-app/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: ["./src/**/*.{html,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme_red': '#84202A',
+        'dark_red': '#28191D',
+        'dark_red': '#28191D',
+        'dark_red': '#28191D',
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
 
