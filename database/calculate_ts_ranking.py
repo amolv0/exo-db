91b04128bb34ae39469c686a52e1afdefc64abdd
ghijj <amolgvaidya@outlook.com>
2024-03-24 09:00:17 -0400

    deleted duplicate rankings and made page size 25

--- database/calculate_ts_ranking.py
+++ database/calculate_ts_ranking.py
@@ -6,7 +6,7 @@ dynamodb = boto3.resource('dynamodb')
 trueskill_table = dynamodb.Table('trueskill-rankings')
 team_table = dynamodb.Table('team-data')
 
-SEASON = 181
+SEASON = 180
 
 def update_team_rankings(season):
 
