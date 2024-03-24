c62da496ac7c684baafb95d498b5b0ee02407eb4
ghijj <amolgvaidya@outlook.com>
2024-03-24 07:05:46 -0400

    updates to elo/ts to have w-l-t and avg_ccwm and rankings api fix

--- database/calculate_ts.py
+++ database/calculate_ts.py
@@ -2,9 +2,10 @@ import boto3
 from boto3.dynamodb.conditions import Key, Attr
 from decimal import Decimal
 import trueskill
+from collections import defaultdict
 
 # Constants
-SEASON_ID = 103
+SEASON_ID = 181
 
 # Initialize TrueSkill environment
 env = trueskill.TrueSkill(draw_probability=0.01)  # Adjust draw_probability as needed
@@ -13,7 +14,7 @@ events_table = dynamodb.Table('event-data')
 teams_table = dynamodb.Table('team-data')
 trueskill_table = dynamodb.Table('trueskill-rankings')
 
-team_ratings = {}  # Global dictionary to store team ratings across events
+team_metrics = defaultdict(lambda: {'wins': 0, 'losses': 0, 'ties': 0, 'ccwms': [], 'events': 1, 'rating': env.create_rating()})
 
 def fetch_events():
     all_event_ids = []
@@ -22,9 +23,8 @@ def fetch_events():
     page = 0
     print("Scanning events")
 
-    # Correct the date range for the query
-    start_date = '2014-03-01T00:00:00-05:00'  # Start of the range
-    end_date = '2015-02-14T00:00:00-05:00'    # End of the range
+    start_date = '2023-04-05T09:30:00-04:00'
+    end_date = '2024-04-01T23:59:00-05:00'
     
     while True:
         page += 1
@@ -33,7 +33,7 @@ def fetch_events():
         query_kwargs = {
             'IndexName': 'EventsByStartDateGSI',
             'KeyConditionExpression': Key('gsiPartitionKey').eq('ALL_EVENTS') & Key('start').between(start_date, end_date),
-            'FilterExpression': Attr('program').is_in(['VEXU'])
+            'FilterExpression': Attr('program').is_in(['VRC'])
         }
 
         if last_evaluated_key:
@@ -59,24 +59,54 @@ def process_events():
     # Log and write the final ratings after processing all events
     log_and_write_final_ratings()
 
-def process_event(event_id):
-    global team_ratings  # Use the global dictionary to track ratings across multiple events
-    
     event_details = fetch_event_details(event_id)
-    if not event_details:
-        print(f"Event details could not be fetched for event ID {event_id}.")
-        return
-    if event_details['season']['id'] != SEASON_ID: return
+    if not event_details or event_details['season']['id'] != SEASON_ID:
+        return  # Skip event if not part of the current season
     
     for division in event_details.get('divisions', []):
-        if 'matches' not in division: continue
+        if 'matches' not in division:
+            continue
+        
         for match in division['matches']:
             winning_team_ids, losing_team_ids, draw_team_ids = determine_match_outcome(match)
-            
-            if (len(winning_team_ids) == 0 or len(losing_team_ids) == 0) and len(draw_team_ids) != 4: continue
-            
-            # Update ratings based on match outcome
             update_trueskill_ratings(winning_team_ids, losing_team_ids, draw_team_ids)
+            
+def process_event(event_id):
+    event_details = fetch_event_details(event_id)
+    if not event_details or event_details['season']['id'] != SEASON_ID:
+        return  # Skip event if not part of the current season
+    
+    for division in event_details.get('divisions', []):
+        if 'matches' not in division:
+            continue
+        
+        for match in division['matches']:
+            winning_team_ids, losing_team_ids, draw_team_ids = determine_match_outcome(match)
+            update_trueskill_ratings(winning_team_ids, losing_team_ids, draw_team_ids)
+        for ranking in division['rankings']:
+            team_id = int(ranking['team']['id'])
+            ccwm = ranking.get('ccwm', 0)
+            high_score = ranking.get('high_score', 0)
+            events = ranking.get('events', 0)
+            
+            if team_id not in team_metrics:
+                team_metrics[team_id] = {
+                    'wins': 0, 
+                    'losses': 0, 
+                    'ties': 0,
+                    'events': events,
+                    'ccwms': [ccwm], 
+                    'high_score': high_score,
+                    'events': 1,
+                    'rating': env.create_rating(),  # Initialize TrueSkill rating if using TrueSkill
+                }
+            else:
+                team_metrics[team_id]['ccwms'].append(ccwm)
+                team_metrics[team_id]['events'] += 1
+                if 'high_score' in team_metrics[team_id]:
+                    team_metrics[team_id]['high_score'] = max(team_metrics[team_id]['high_score'], high_score)
+                else:
+                    team_metrics[team_id]['high_score'] = high_score
 
 def fetch_event_details(event_id):
     try:
@@ -91,12 +121,17 @@ def determine_match_outcome(match):
     Determine the winning, losing teams, and draws based on the score.
     Returns a tuple of lists: (winning_team_ids, losing_team_ids, draw_team_ids).
     In the case of a draw, both winning_team_ids and losing_team_ids will be empty, and draw_team_ids will contain the IDs of all teams involved in the match.
+    In the case where both scores are 0, it's assumed the match hasn't taken place yet and all return lists will be empty.
     """
     alliances = match['alliances']
     scores = {alliance['color']: (int(alliance['score']), [team['team']['id'] for team in alliance['teams']]) for alliance in alliances}
     
+    # Check if match hasn't taken place yet
+    if scores['red'][0] == 0 and scores['blue'][0] == 0:
+        return [], [], []  # Match hasn't taken place yet
+
     # Check if it's a draw
-    if scores['red'][0] == scores['blue'][0]:
+    elif scores['red'][0] == scores['blue'][0]:
         # It's a draw, combine both teams
         draw_team_ids = scores['red'][1] + scores['blue'][1]
         return [], [], draw_team_ids
@@ -111,82 +146,100 @@ def determine_match_outcome(match):
         return winning_team_ids, losing_team_ids, []
 
 def update_trueskill_ratings(winning_team_ids, losing_team_ids, draw_team_ids):
-    global team_ratings
+    global team_metrics, env
 
+    # Preparing the team ratings for update
+    for team_id in winning_team_ids + losing_team_ids + draw_team_ids:
+        if team_id not in team_metrics:
+            team_metrics[team_id]['rating'] = env.create_rating()
+
+    # Handling win/lose scenarios
     if winning_team_ids and losing_team_ids:
-        # Handle win/lose scenario
-        winners = [team_ratings.setdefault(team_id, env.create_rating()) for team_id in winning_team_ids]
-        losers = [team_ratings.setdefault(team_id, env.create_rating()) for team_id in losing_team_ids]
-        updated_ratings = env.rate([winners, losers], ranks=[0, 1])
-        for team_id, new_rating in zip(winning_team_ids, updated_ratings[0]):
-            team_ratings[team_id] = trueskill.Rating(mu=round(new_rating.mu, 2), sigma=round(new_rating.sigma, 2))
-        for team_id, new_rating in zip(losing_team_ids, updated_ratings[1]):
-            team_ratings[team_id] = trueskill.Rating(mu=round(new_rating.mu, 2), sigma=round(new_rating.sigma, 2))
+        winners = [team_metrics[team_id]['rating'] for team_id in winning_team_ids]
+        losers = [team_metrics[team_id]['rating'] for team_id in losing_team_ids]
+        updated_winners, updated_losers = env.rate([winners, losers], ranks=[0, 1])
+        
+        for i, team_id in enumerate(winning_team_ids):
+            team_metrics[team_id]['rating'] = updated_winners[i]
+            team_metrics[team_id]['wins'] += 1
+        
+        for i, team_id in enumerate(losing_team_ids):
+            team_metrics[team_id]['rating'] = updated_losers[i]
+            team_metrics[team_id]['losses'] += 1
+
+    # Correct handling of draws
     elif draw_team_ids:
-        # Handle draw scenario
-        # Split draw teams into two groups for TrueSkill processing
-        half_size = len(draw_team_ids) // 2
-        draw_group_1 = [team_ratings.setdefault(team_id, env.create_rating()) for team_id in draw_team_ids[:half_size]]
-        draw_group_2 = [team_ratings.setdefault(team_id, env.create_rating()) for team_id in draw_team_ids[half_size:]]
-        updated_ratings = env.rate([draw_group_1, draw_group_2], ranks=[0, 0])
-        draw_teams_combined = draw_team_ids[:half_size] + draw_team_ids[half_size:]
-        updated_ratings_combined = updated_ratings[0] + updated_ratings[1]
-        for team_id, new_rating in zip(draw_teams_combined, updated_ratings_combined):
-            team_ratings[team_id] = trueskill.Rating(mu=round(new_rating.mu, 2), sigma=round(new_rating.sigma, 2))
+        draw_teams = [team_metrics[team_id]['rating'] for team_id in draw_team_ids]
+        # To rate a draw, split the teams into two groups and assign them the same rank
+        half = len(draw_teams) // 2
+        group1 = draw_teams[:half] or draw_teams  # Ensure there's at least one team in group1
+        group2 = draw_teams[half:] or draw_teams  # Ensure there's at least one team in group2
+        if group1 and group2:  # Check if both groups have members
+            updated_ratings = env.rate([group1, group2], ranks=[0, 0])
+            for i, team_id in enumerate(draw_team_ids[:half]):
+                team_metrics[team_id]['rating'] = updated_ratings[0][i]
+            for i, team_id in enumerate(draw_team_ids[half:]):
+                team_metrics[team_id]['rating'] = updated_ratings[1][i] if half else updated_ratings[0][i]
+            for team_id in draw_team_ids:
+                team_metrics[team_id]['ties'] += 1
 
    
-def fetch_team_data(team_ids):
-    team_regions = {}
-    team_names = {}
-    team_numbers = {}
-    team_orgs = {}
+def fetch_additional_team_data(team_ids):
+    """Fetch additional team data like number, name, organization, and region."""
+    team_data = {}
     for team_id in team_ids:
-        try:
-            response = teams_table.get_item(Key={'id': team_id})
-            if 'Item' in response:
-                if 'region' in response['Item']:
-                    team_regions[team_id] = response['Item']['region']
-                if 'team_name' in response['Item']:
-                    team_names[team_id] = response['Item']['team_name']
-                if 'organization' in response['Item']:
-                    team_orgs[team_id] = response['Item']['organization']
-                if 'number' in response['Item']:
-                    team_numbers[team_id] = response['Item']['number']
-            else:
-                team_regions[team_id] = "Unknown"
-        except Exception as e:
-            print(f"Error fetching region for team ID {team_id}: {e}")
-            team_regions[team_id] = "Error"
-    return team_regions, team_names, team_numbers, team_orgs
+        response = teams_table.get_item(Key={'id': team_id})
+        if 'Item' in response:
+            item = response['Item']
+            team_data[team_id] = {
+                'team_number': item.get('number', 'Unknown'),
+                'team_name': item.get('team_name', 'Unknown'),
+                'team_org': item.get('organization', 'Unknown'),
+                'region': item.get('region', 'Unknown'),
+            }
+        else:
+            team_data[team_id] = {
+                'team_number': 'Unknown',
+                'team_name': 'Unknown',
+                'team_org': 'Unknown',
+                'region': 'Unknown',
+            }
+    return team_data
 
 def log_and_write_final_ratings():
-    global team_ratings
-    team_regions, team_names, team_numbers, team_orgs = fetch_team_data(team_ratings.keys())  # Fetch all team regions at once
+    global team_metrics
+    
+    # Fetch additional team data
+    team_ids = list(team_metrics.keys())
+    additional_team_data = fetch_additional_team_data(team_ids)
 
-    log_file_path = "logs/trueskill.log"
-    with open(log_file_path, "w") as file, trueskill_table.batch_writer() as batch:
-        file.write("Final TrueSkill Ratings:\n")
-        for team_id, rating in team_ratings.items():
-            region = team_regions.get(team_id, "Unknown")
-            team_name = team_names.get(team_id, "Unknown")
-            team_number = team_numbers.get(team_id, "Unknown")
-            team_org = team_orgs.get(team_id, "Unknown")
-            file.write(f"Team ID: {team_id}, Mu: {rating.mu}, Sigma: {rating.sigma}, Region: {region}\n")
+    with trueskill_table.batch_writer() as batch:
+        for team_id, metrics in team_metrics.items():
+            data = additional_team_data.get(team_id, {})
+            avg_ccwm = sum(metrics['ccwms']) / len(metrics['ccwms']) if metrics['ccwms'] else Decimal(0)
+            high_score = Decimal(str(metrics.get('high_score', 0)))
+            
             batch.put_item(
                 Item={
                     'season-team': f"{SEASON_ID}-{team_id}",
-                    'mu': Decimal(str(rating.mu)),
-                    'sigma': Decimal(str(rating.sigma)),
-                    'team_id': team_id,
-                    'team_name': team_name,
-                    'team_number': team_number,
-                    'team_org': team_org,
-                    'season': SEASON_ID,
-                    'region': region  # Include region in the item
+                    'mu': Decimal(str(metrics['rating'].mu)),
+                    'sigma': Decimal(str(metrics['rating'].sigma)),
+                    'wins': Decimal(str(metrics['wins'])),
+                    'losses': Decimal(str(metrics['losses'])),
+                    'ties': Decimal(str(metrics['ties'])),
+                    'events': Decimal(str(metrics['events'])),
+                    'ccwms': metrics['ccwms'],
+                    'avg_ccwm': Decimal(str(avg_ccwm)),
+                    'high_score': Decimal(str(high_score)),
+                    'team_id': Decimal(str(team_id)),
+                    'team_number': data.get('team_number', 'Unknown'),
+                    'team_name': data.get('team_name', 'Unknown'),
+                    'team_org': data.get('team_org', 'Unknown'),
+                    'season': Decimal(str(SEASON_ID)),
+                    'region': data.get('region', 'Unknown'),
                 }
             )
-            
-            # update_team_trueskill(team_id, rating.mu, rating.sigma, SEASON_ID)
+    print("All metrics logged and updated in DynamoDB.")
             
 def update_team_trueskill(team_id, mu, sigma, season_id):
     global count
@@ -218,7 +271,7 @@ def update_team_trueskill(team_id, mu, sigma, season_id):
             print(f"Error initializing teamskill for team {team_id}: {e}")
 
 count = 0
-print(team_ratings)
+# print(team_metrics)
 
 # process_event(53340)
 process_events()
