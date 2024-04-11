c1e1f89dfb120d37fd806f56fc8f797e26d8ddc1
Ray <rguo1010@uw.edu>
2024-04-10 18:28:21 -0700

    More ui changes

--- web-app/src/components/EventInfo/EventLocation.tsx
+++ web-app/src/components/EventInfo/EventLocation.tsx
@@ -1,100 +1,104 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { Box, Typography, Link } from '@mui/material';
+import { Link, Typography } from '@mui/material';
 
-// Define your interfaces here...
-
-interface SeasonData {
-  name: string;
-  id: number;
-  code: string | null;
+interface AwardWinner {
+    team: {
+        id: string;
+        name: string;
+    };
 }
 
-interface LocationData {
-  venue: string | null;
-  country: string | null;
-  city: string | null;
-  address_1: string;
-  address_2: string | null;
-  postcode: string;
-  coordinates: { lat: number; lon: number };
-  region: string;
-}
-
-interface TeamData {
-  name: string;
-  id: number;
-}
-
-interface WinnersData {
-  team: TeamData;
-}
-
-interface AwardData {
-  title: string;
-  teamWinners: WinnersData[];
+interface Award {
+    id: string;
+    title: string;
+    teamWinners?: AwardWinner[];
 }
 
 interface JSONComponentProps {
-  location: LocationData | null;
-  season: SeasonData | null;
-  program: string | null;
-  awards: AwardData[] | null;
+    location?: string;
+    season?: string;
+    program?: string;
+    awards?: Award[];
 }
 
-const JSONComponent: React.FC<JSONComponentProps> = ({ location, season, program, awards }) => {
-  return (
-    <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: 2 }}>
-      <Box sx={{ m: 2, bgcolor: 'grey.800', color: 'white', p: 2, borderRadius: 2, boxShadow: 3, mt: 2 }}>
-        <Typography variant="h6" sx={{ mb: 2 }}>Event Data</Typography>
-        {location && (
-          <Box sx={{ mb: 3 }}>
-            <Typography variant="subtitle1" sx={{ mb: 1 }}>Location</Typography>
-            <Typography>Venue: {location.venue || 'N/A'}</Typography>
-            <Typography>Country: {location.country || 'N/A'}</Typography>
-            <Typography>City: {location.city || 'N/A'}</Typography>
-            <Typography>Address: {location.address_1 || 'N/A'}</Typography>
-            <Typography>Region: {location.region || 'N/A'}</Typography>
-          </Box>
-        )}
-        {season && (
-          <Box sx={{ mb: 3 }}>
-            <Typography variant="subtitle1" sx={{ mb: 1 }}>Season</Typography>
-            <Typography>{season.name || 'N/A'}</Typography>
-          </Box>
-        )}
-        {program && (
-          <Box sx={{ mb: 3 }}>
-            <Typography variant="subtitle1" sx={{ mb: 1 }}>Program</Typography>
-            <Typography>{program || 'N/A'}</Typography>
-          </Box>
-        )}
-      </Box>
-      <Box sx={{ m: 2, bgcolor: 'grey.800', color: 'white', p: 2, borderRadius: 2, boxShadow: 3, mt: 2 }}>
-        {awards && awards.length > 0 && (
-          <Box>
-            <Typography variant="h6" sx={{ mb: 2 }}>Award Winners</Typography>
-            {awards.map((award, index) => (
-              <Box key={index} sx={{ mb: 3 }}>
-                <Typography variant="subtitle1" sx={{ mb: 1 }}>
-                  {award.title && award.title.substring(0, award.title.indexOf('('))}
-                </Typography>
-                <Box sx={{ display: 'flex' }}>
-                  {award.teamWinners && award.teamWinners.map((winner, winnerIndex) => (
-                    <Box key={winnerIndex} sx={{ mr: 2 }}>
-                      <Link component={RouterLink} to={`/teams/${winner.team.id}`} sx={{ color: 'lightblue', '&:hover': { color: 'blue' } }}>
-                        {winner.team.name}
-                      </Link>
-                    </Box>
-                  ))}
-                </Box>
-              </Box>
-            ))}
-          </Box>
-        )}
-      </Box>
-    </Box>
-  );
+const JSONComponent: React.FC<JSONComponentProps> = ({ awards }) => {
+    const championExcellenceAwards = (awards ?? []).filter((award) =>
+        award.title?.includes('Champion') || award.title?.includes('Excellence')
+    );
+
+    const otherAwards = (awards ?? []).filter((award) =>
+        !award.title?.includes('Champion') && !award.title?.includes('Excellence')
+        && award.title?.includes('Award') && !award.title?.includes('Volunteer')
+    );
+
+    function chunkArray<T>(array: T[], itemsPerRow: number): T[][] {
+      return Array.from({ length: Math.ceil(array.length / itemsPerRow) }, (_, index) =>
+          array.slice(index * itemsPerRow, (index + 1) * itemsPerRow)
+      );
+  }
+
+    return (
+        <div>
+            {championExcellenceAwards.length > 0 && (
+                <div>
+                    <div className="event-profile-subtitle champion">Champions</div>
+                    <div className="event-profile-info">
+                        {championExcellenceAwards.map((award) => (
+                            <div className="event-profile-row" key={award.id}>
+                                <div className="event-profile-label">{award.title && award.title.substring(0, award.title.indexOf('(') - 1)}</div>
+                                <div>
+                                    {award.teamWinners &&
+                                        award.teamWinners.map((winner) => (
+                                            <Link
+                                                key={winner.team.id}
+                                                component={RouterLink}
+                                                to={`/teams/${winner.team.id}`}
+                                            >
+                                                <div className="event-profile-value">
+                                                    {winner.team.name}
+                                                </div>
+                                            </Link>
+                                        ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+
+            {otherAwards.length > 0 && (
+              <div>
+                  <div className="event-profile-subtitle">Awards</div>
+                  <div className="event-profile-info">
+                      {chunkArray(otherAwards, 2).map((group, groupIndex) => (
+                          <div key={groupIndex}>
+                              {group.map((award) => (
+                                  <div className="event-profile-row" key={award.id}>
+                                      <div className="event-profile-label">{award.title && award.title.substring(0, award.title.indexOf('(') - 1)}</div>
+                                      <div>
+                                          {award.teamWinners &&
+                                              award.teamWinners.map((winner) => (
+                                                  <Link
+                                                      key={winner.team.id}
+                                                      component={RouterLink}
+                                                      to={`/teams/${winner.team.id}`}
+                                                  >
+                                                      <div className="event-profile-value">
+                                                          {winner.team.name}
+                                                      </div>
+                                                  </Link>
+                                              ))}
+                                      </div>
+                                  </div>
+                              ))}
+                          </div>
+                      ))}
+                  </div>
+              </div>
+          )}
+        </div>
+    );
 };
 
 export default JSONComponent;
