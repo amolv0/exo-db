62337ff02f576631bbb506e38460004a269a587f
Ray <rguo1010@uw.edu>
2024-04-13 20:46:51 -0700

    Various different bugfixes + updates

--- web-app/src/components/EventInfo/EventSkills.tsx
+++ web-app/src/components/EventInfo/EventSkills.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import '../../Stylesheets/eventTable.css';
 
 interface TeamData {
   name: string;
@@ -20,103 +21,100 @@ interface EventSkillsComponentProps {
 }
 
 const EventSkillsComponent: React.FC<EventSkillsComponentProps> = ({ skills }) => {
-  const [selectedOption, setSelectedOption] = useState<'programming' | 'driver' | 'combined'>('combined');
+  
+const [selectedOption, setSelectedOption] = useState<'programming' | 'driver' | 'combined'>('combined');
 
-  const programmingSkills = skills.filter(skill => skill.type === "programming");
-  const driverSkills = skills.filter(skill => skill.type === "driver");
+const programmingSkills = skills.filter(skill => skill.type === "programming");
+const driverSkills = skills.filter(skill => skill.type === "driver");
 
-  const programmingScoresMap = new Map<number, number>();
-  const driverScoresMap = new Map<number, number>();
-  const combinedScoresMap = new Map<number, number>();
-  
-  programmingSkills.forEach(skill => {
-    const teamId = skill.team.id;
-    if (!programmingScoresMap.has(teamId)) {
-      programmingScoresMap.set(teamId, skill.score);
-    } else {
-      programmingScoresMap.set(teamId, programmingScoresMap.get(teamId)! + skill.score);
-    }
-  });
-  
-  driverSkills.forEach(skill => {
-    const teamId = skill.team.id;
-    if (!driverScoresMap.has(teamId)) {
-      driverScoresMap.set(teamId, skill.score);
-    } else {
-      driverScoresMap.set(teamId, driverScoresMap.get(teamId)! + skill.score);
-    }
-  });
-  
-  skills.forEach(skill => {
-    const teamId = skill.team.id;
-    const combinedScore = (programmingScoresMap.get(teamId) ?? 0) + (driverScoresMap.get(teamId) ?? 0);
-    combinedScoresMap.set(teamId, combinedScore);
-  });
-  
-  const sortedCombinedScores = Array.from(combinedScoresMap.entries()).sort((a, b) => b[1] - a[1]);
-  const sortedProgScores = Array.from(programmingScoresMap.entries()).sort((a, b) => b[1] - a[1]);
-  const sortedDriverScores = Array.from(driverScoresMap.entries()).sort((a, b) => b[1] - a[1]);
+const programmingSkillsMap = new Map<number, Skill[]>();
+const driverSkillsMap = new Map<number, Skill[]>();
+const combinedSkillsMap = new Map<number, Skill[]>();
+
+programmingSkills.forEach(skill => {
+  const teamId = skill.team.id;
+  const existingSkills = programmingSkillsMap.get(teamId) || [];
+  programmingSkillsMap.set(teamId, [...existingSkills, skill]);
+});
+
+driverSkills.forEach(skill => {
+  const teamId = skill.team.id;
+  const existingSkills = driverSkillsMap.get(teamId) || [];
+  driverSkillsMap.set(teamId, [...existingSkills, skill]);
+});
+
+skills.forEach(skill => {
+  const teamId = skill.team.id;
+  const combinedSkills = [
+    ...(programmingSkillsMap.get(teamId) || []),
+    ...(driverSkillsMap.get(teamId) || [])
+  ];
+  combinedSkillsMap.set(teamId, combinedSkills);
+});
+
+const sortedCombinedSkills = Array.from(combinedSkillsMap.entries()).sort((a, b) => {
+  const totalScoreA = a[1].reduce((acc, curr) => acc + curr.score, 0);
+  const totalScoreB = b[1].reduce((acc, curr) => acc + curr.score, 0);
+  return totalScoreB - totalScoreA;
+});
+const sortedProgSkills = Array.from(programmingSkillsMap.entries()).sort((a, b) => {
+  const totalScoreA = a[1].reduce((acc, curr) => acc + curr.score, 0);
+  const totalScoreB = b[1].reduce((acc, curr) => acc + curr.score, 0);
+  return totalScoreB - totalScoreA;
+});
+const sortedDriverSkills = Array.from(driverSkillsMap.entries()).sort((a, b) => {
+  const totalScoreA = a[1].reduce((acc, curr) => acc + curr.score, 0);
+  const totalScoreB = b[1].reduce((acc, curr) => acc + curr.score, 0);
+  return totalScoreB - totalScoreA;
+});
+
+console.log(sortedProgSkills);
   
 
   return (
-    <div className="mx-auto bg-gray-700 text-white p-4 rounded-lg shadow-md mt-4">
-      <div className="flex flex-row space-x-4">
-        <button className={`rounded-lg px-4 py-2 ${selectedOption === 'combined' ? 'bg-blue-200 text-white' : 'bg-gray-200 text-gray-800'}`} onClick={() => setSelectedOption('combined')}>Combined Scores</button>
-        <button className={`rounded-lg px-4 py-2 ${selectedOption === 'driver' ? 'bg-blue-200 text-white' : 'bg-gray-200 text-gray-800'}`} onClick={() => setSelectedOption('driver')}>Driver Scores</button>
-        <button className={`rounded-lg px-4 py-2 ${selectedOption === 'programming' ? 'bg-blue-200 text-white' : 'bg-gray-200 text-gray-800'}`} onClick={() => setSelectedOption('programming')}>Programming Scores</button>
-      </div>
-      <div>
-        {selectedOption === 'programming' && (
-          <ul className="ml-4">
-            {sortedProgScores.map(([teamId, score], index) => {
-              const team = skills.find(skill => skill.team.id === teamId)?.team;
-              if (team) {
-                return (
-                  <li key={teamId} className="mb-4 border-b border-gray-300 py-2">
-                    <Link to={`/teams/${teamId}`} className="hover:text-blue-500">
-                      <span className="font-semibold">{index + 1}.</span> {team.name} - Score: {score}
-                    </Link>
-                  </li>
-                );
-              }
-              return null;
-            })}
-          </ul>
-        )}
-        {selectedOption === 'driver' && (
-          <ul className="ml-4">
-            {sortedDriverScores.map(([teamId, score], index) => {
-              const team = skills.find(skill => skill.team.id === teamId)?.team;
-              if (team) {
-                return (
-                  <li key={teamId} className="mb-4 border-b border-gray-300 py-2">
-                    <Link to={`/teams/${teamId}`} className="hover:text-blue-500">
-                      <span className="font-semibold">{index + 1}.</span> {team.name} - Score: {score}
-                    </Link>
-                  </li>
-                );
-              }
-              return null;
-            })}
-          </ul>
-        )}
-        {selectedOption === 'combined' && (
-          <ul className="ml-4">
-            {sortedCombinedScores.map(([teamId, score], index) => {
-              const team = skills.find(skill => skill.team.id === teamId)?.team;
-              if (team) {
-                return (
-                  <li key={teamId} className="mb-4 border-b border-gray-300 py-2">
-                    <Link to={`/teams/${teamId}`} className="hover:text-blue-500">
-                      <span className="font-semibold">{index + 1}.</span> {team.name} - Score: {score}
-                    </Link>
-                  </li>
-                );
-              }
-              return null;
-            })}
-          </ul>
-        )}
+    <div className = "p-10">
+      <div className="skillsListsTitle">Skills List</div>  
+      <div className="skillsTable">
+        <div className="header col x-small">
+            <div className = "header-cell rounded-tl-lg">
+            Rank
+            </div>
+            {sortedCombinedSkills && Array.isArray(sortedCombinedSkills) && sortedCombinedSkills.map(([teamId, skills], index, array) => (
+              <div className={`body-cell ${index % 2 === 0 ? 'bg-opacity-65' : ''} ${index === array.length - 1 ? 'rounded-bl-lg rounded-b-none' : ''}`}>
+                  {index + 1}
+              </div>
+            ))}
+        </div>
+        <div className="header col score">
+            <div className = "header-cell">
+            Score
+            </div>
+            {sortedCombinedSkills && Array.isArray(sortedCombinedSkills) && sortedCombinedSkills.map(([teamId, skills], index, array) => (
+                <div className={`body-cell ${index % 2 === 0 ? 'bg-opacity-65' : ''}`}>
+                <div className = "flex gap-2 items-center justify-center">
+                  <div className = "scoreDisplay">
+                    {skills[0].score + skills[1].score}
+                  </div> 
+                  <div>
+                    (D: {skills[0].score})
+                    (P: {skills[1].score})
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+        <div className="header col rankings-t">
+            <div className = "header-cell">
+            Number
+            </div>
+            {sortedCombinedSkills && Array.isArray(sortedCombinedSkills) && sortedCombinedSkills.map(([teamId, skills], index, array) => (
+              <div className={`body-cell ${index % 2 === 0 ? 'bg-opacity-65' : ''} ${index === array.length - 1 ? 'rounded-bl-lg rounded-b-none' : ''}`}>
+                  <Link className="teamBox" to={`/teams/${teamId}`}>
+                      {skills[0].team.name}
+                  </Link>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );  
