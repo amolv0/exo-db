2d300f14ce7c23b9a4308bb75b67a7a5350c436d
Ray <rguo1010@uw.edu>
2024-04-14 21:13:11 -0700

    Lots of text changes + env

--- web-app/src/components/Dropdowns/DivisionDropDown.tsx
+++ web-app/src/components/Dropdowns/DivisionDropDown.tsx
@@ -1,37 +1,43 @@
 import React from 'react';
 import '../../Stylesheets/dropdown.css';
 
+// This represents the dropdown to select the division a team is in
+
+// Extract the current division, the possible divisions, setDivision function
 interface DivisionProps {
-  setSelectedDivision: React.Dispatch<React.SetStateAction<number>>;
-  division: number;
-  divisions: {name : string}[];
+    division: number;
+    divisions: {name : string}[];
+    setSelectedDivision: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const StartAfterDateInput: React.FC<DivisionProps> = ({ setSelectedDivision, division, divisions}) => {
-  const handleDivisionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedDivision = parseInt(event.target.value, 10);
-    setSelectedDivision(selectedDivision);
-  };
+const DivisionDropDown: React.FC<DivisionProps> = ({ setSelectedDivision, division, divisions}) => {
 
-  return (
-    <div className="filter">
-      <div className="query">Divisions</div>
-      <div className="search-filter">
-        <select
-            id="division"
-            value={division}
-            onChange={handleDivisionChange}
-            style={{ width: 'auto', height: '30px' }}
-        >
-          {divisions.map((division, index) => (
-            <option key={index} value={index}>
-              {division.name}
-            </option>
-          ))}
-        </select>
-      </div>
-    </div>
-  );
+    const handleDivisionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedDivision = parseInt(event.target.value, 10);
+        setSelectedDivision(selectedDivision);
+    };
+
+    return (
+        <div className="filter">
+            <div className="query">
+                Divisions
+            </div>
+            <div className="search-filter">
+                <select
+                    id="division"
+                    value={division}
+                    onChange={handleDivisionChange}
+                    style={{ width: 'auto', height: '30px' }}
+                >
+                    {divisions.map((division, index) => (
+                      <option key={index} value={index}>
+                        {division.name}
+                      </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
 };
 
-export default StartAfterDateInput;
+export default DivisionDropDown;
--- web-app/src/components/EventInfo/EventSkills.tsx
+++ web-app/src/components/EventInfo/EventSkills.tsx
@@ -2,122 +2,131 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../../Stylesheets/eventTable.css';
 
+// Displays the skills Rankings for the event
+
 interface TeamData {
-  name: string;
-  id: number;
+    name: string;
+    id: number;
 }
 
-interface Skill {
-  score: number;
-  name: string;
-  rank: number;
-  team: TeamData;
-  type: string;
-  attempts: number;
+interface SkillData {
+    score: number;
+    name: string;
+    rank: number;
+    team: TeamData;
+    type: string;
+    attempts: number;
 }
 
 interface EventSkillsComponentProps {
-  skills: Skill[];
+    skills: SkillData[];
 }
 
 const EventSkillsComponent: React.FC<EventSkillsComponentProps> = ({ skills }) => {
   
-const [selectedOption, setSelectedOption] = useState<'programming' | 'driver' | 'combined'>('combined');
+    const [selectedOption, setSelectedOption] = useState<'programming' | 'driver' | 'combined'>('combined');
 
-const programmingSkills = skills.filter(skill => skill.type === "programming");
-const driverSkills = skills.filter(skill => skill.type === "driver");
+    // Filter the "programming" scores and "driver" scores, and need to map the
+    // team to their prog + driver scores because data is not given together
+    
+    const programmingSkills = skills.filter(skill => skill.type === "programming");
+    const driverSkills = skills.filter(skill => skill.type === "driver");
 
-const programmingSkillsMap = new Map<number, Skill[]>();
-const driverSkillsMap = new Map<number, Skill[]>();
-const combinedSkillsMap = new Map<number, Skill[]>();
+    const programmingSkillsMap = new Map<number, SkillData[]>();
+    const driverSkillsMap = new Map<number, SkillData[]>();
+    const combinedSkillsMap = new Map<number, SkillData[]>();
 
-programmingSkills.forEach(skill => {
-  const teamId = skill.team.id;
-  const existingSkills = programmingSkillsMap.get(teamId) || [];
-  programmingSkillsMap.set(teamId, [...existingSkills, skill]);
-});
+    programmingSkills.forEach(skill => {
+        const teamId = skill.team.id;
+        const existingSkills = programmingSkillsMap.get(teamId) || [];
+        programmingSkillsMap.set(teamId, [...existingSkills, skill]);
+    });
 
-driverSkills.forEach(skill => {
-  const teamId = skill.team.id;
-  const existingSkills = driverSkillsMap.get(teamId) || [];
-  driverSkillsMap.set(teamId, [...existingSkills, skill]);
-});
+    driverSkills.forEach(skill => {
 
-skills.forEach(skill => {
-  const teamId = skill.team.id;
-  const combinedSkills = [
-    ...(programmingSkillsMap.get(teamId) || []),
-    ...(driverSkillsMap.get(teamId) || [])
-  ];
-  combinedSkillsMap.set(teamId, combinedSkills);
-});
+        const teamId = skill.team.id;
+        const existingSkills = driverSkillsMap.get(teamId) || [];
+        driverSkillsMap.set(teamId, [...existingSkills, skill]);
+    });
 
-const sortedCombinedSkills = Array.from(combinedSkillsMap.entries()).sort((a, b) => {
-  const totalScoreA = a[1].reduce((acc, curr) => acc + curr.score, 0);
-  const totalScoreB = b[1].reduce((acc, curr) => acc + curr.score, 0);
-  return totalScoreB - totalScoreA;
-});
-const sortedProgSkills = Array.from(programmingSkillsMap.entries()).sort((a, b) => {
-  const totalScoreA = a[1].reduce((acc, curr) => acc + curr.score, 0);
-  const totalScoreB = b[1].reduce((acc, curr) => acc + curr.score, 0);
-  return totalScoreB - totalScoreA;
-});
-const sortedDriverSkills = Array.from(driverSkillsMap.entries()).sort((a, b) => {
-  const totalScoreA = a[1].reduce((acc, curr) => acc + curr.score, 0);
-  const totalScoreB = b[1].reduce((acc, curr) => acc + curr.score, 0);
-  return totalScoreB - totalScoreA;
-});
+    skills.forEach(skill => {
+        const teamId = skill.team.id;
+        const combinedSkills = [
+          ...(programmingSkillsMap.get(teamId) || []),
+          ...(driverSkillsMap.get(teamId) || [])
+        ];
+        combinedSkillsMap.set(teamId, combinedSkills);
+    });
 
-console.log(sortedProgSkills);
-  
+    // Sort the prog + driver skills
+    const sortedCombinedSkills = Array.from(combinedSkillsMap.entries()).sort((a, b) => {
+        const totalScoreA = a[1].reduce((acc, curr) => acc + curr.score, 0);
+        const totalScoreB = b[1].reduce((acc, curr) => acc + curr.score, 0);
+        return totalScoreB - totalScoreA;
+    });
 
-  return (
-    <div className = "p-10">
-      <div className="skillsListsTitle">Skills List</div>  
-      <div className="skillsTable">
-        <div className="header col x-small">
-            <div className = "header-cell rounded-tl-lg">
-            Rank
-            </div>
-            {sortedCombinedSkills && Array.isArray(sortedCombinedSkills) && sortedCombinedSkills.map(([teamId, skills], index, array) => (
-              <div className={`body-cell ${index % 2 === 0 ? 'bg-opacity-65' : ''} ${index === array.length - 1 ? 'rounded-bl-lg rounded-b-none' : ''}`}>
-                  {index + 1}
-              </div>
-            ))}
-        </div>
-        <div className="header col score">
-            <div className = "header-cell">
-            Score
-            </div>
-            {sortedCombinedSkills && Array.isArray(sortedCombinedSkills) && sortedCombinedSkills.map(([teamId, skills], index, array) => (
-                <div className={`body-cell ${index % 2 === 0 ? 'bg-opacity-65' : ''}`}>
-                <div className = "flex gap-2 items-center justify-center">
-                  <div className = "scoreDisplay">
-                    {skills[0].score + skills[1].score}
-                  </div> 
-                  <div>
-                    (D: {skills[0].score})
-                    (P: {skills[1].score})
-                  </div>
+    // Sort the prog skills
+    const sortedProgSkills = Array.from(programmingSkillsMap.entries()).sort((a, b) => {
+        const totalScoreA = a[1].reduce((acc, curr) => acc + curr.score, 0);
+        const totalScoreB = b[1].reduce((acc, curr) => acc + curr.score, 0);
+        return totalScoreB - totalScoreA;
+    });
+
+    // Sort the driver skills
+    const sortedDriverSkills = Array.from(driverSkillsMap.entries()).sort((a, b) => {
+        const totalScoreA = a[1].reduce((acc, curr) => acc + curr.score, 0);
+        const totalScoreB = b[1].reduce((acc, curr) => acc + curr.score, 0);
+        return totalScoreB - totalScoreA;
+    });
+
+    return (
+        <div className = "p-10">
+            <div className="skillsListsTitle">Skills List</div>  
+            
+            <div className="skillsTable">
+                <div className="header col x-small">
+                    <div className = "header-cell rounded-tl-lg">
+                        Rank
+                    </div>
+                    {sortedCombinedSkills && Array.isArray(sortedCombinedSkills) && sortedCombinedSkills.map(([teamId, skills], index, array) => (
+                        <div className={`body-cell ${index % 2 === 0 ? 'bg-opacity-65' : ''} ${index === array.length - 1 ? 'rounded-bl-lg rounded-b-none' : ''}`}>
+                            {index + 1}
+                        </div>
+                    ))}
                 </div>
-              </div>
-            ))}
-        </div>
-        <div className="header col rankings-t">
-            <div className = "header-cell">
-            Number
+                <div className="header col score">
+                    <div className = "header-cell">
+                        Score
+                    </div>
+                    {sortedCombinedSkills && Array.isArray(sortedCombinedSkills) && sortedCombinedSkills.map(([teamId, skills], index, array) => (
+                        <div className={`body-cell ${index % 2 === 0 ? 'bg-opacity-65' : ''}`}>
+                            <div className = "flex gap-2 items-center justify-center">
+                                <div className = "scoreDisplay">
+                                    {skills[0].score + skills[1].score}
+                                </div> 
+                                <div>
+                                    (D: {skills[0].score})
+                                    (P: {skills[1].score})
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="header col rankings-t">
+                    <div className = "header-cell">
+                        Number
+                    </div>
+                    {sortedCombinedSkills && Array.isArray(sortedCombinedSkills) && sortedCombinedSkills.map(([teamId, skills], index, array) => (
+                        <div className={`body-cell ${index % 2 === 0 ? 'bg-opacity-65' : ''} ${index === array.length - 1 ? 'rounded-bl-lg rounded-b-none' : ''}`}>
+                            <Link className="teamBox" to={`/teams/${teamId}`}>
+                                {skills[0].team.name}
+                            </Link>
+                        </div>
+                    ))}
+                </div>
             </div>
-            {sortedCombinedSkills && Array.isArray(sortedCombinedSkills) && sortedCombinedSkills.map(([teamId, skills], index, array) => (
-              <div className={`body-cell ${index % 2 === 0 ? 'bg-opacity-65' : ''} ${index === array.length - 1 ? 'rounded-bl-lg rounded-b-none' : ''}`}>
-                  <Link className="teamBox" to={`/teams/${teamId}`}>
-                      {skills[0].team.name}
-                  </Link>
-              </div>
-            ))}
         </div>
-      </div>
-    </div>
-  );  
+    );  
 }
 
 export default EventSkillsComponent;
