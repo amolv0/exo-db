94f0aecd8b99bd70f2e6deb48fc3a606f59bc6a5
Ray <rguo1010@uw.edu>
2024-04-14 21:13:11 -0700

    Lots of text changes + env

--- web-app/src/pages/Rankings.tsx
+++ web-app/src/pages/Rankings.tsx
@@ -1,62 +1,59 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-import SeasonRankings from '../components/RankingsList/SeasonRankings';
+import RatingsList from '../components/Lists/RatingsList';
 import RegionDropdown from '../components/Dropdowns/RegionDropDown';
 import SeasonDropdown from '../components/Dropdowns/SeasonDropDown';
 import ProgramDropdown from '../components/Dropdowns/ProgramDropDown';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import '../Stylesheets/pageLayout.css';
 
+// Controls display for general rankings page
+
 const Rankings: React.FC = () => {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const [seasonId, setSeasonId] = useState<number>(parseInt(searchParams.get('seasonId') || '181'));
-  const [program, setProgram] = useState<string>(searchParams.get('program')  || 'VRC'); 
-  const [selectedRegion, setSelectedRegion] = useState<string>(searchParams.get('region') || '');
-  const [loading, setLoading] = useState<boolean>(true);
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const [seasonId, setSeasonId] = useState<number>(parseInt(searchParams.get('seasonId') || '181'));
+    const [program, setProgram] = useState<string>(searchParams.get('program')  || 'VRC'); 
+    const [selectedRegion, setSelectedRegion] = useState<string>(searchParams.get('region') || '');
+    
+    const initialRender = useRef(true);
+
+    // If the program changes, hardforce the program change
+    useEffect(() => {
+        if (initialRender.current) {
+            initialRender.current = false;
+            return;
+        }
+
+        if (program === 'VRC') {
+            setSeasonId(181);
+        } else {
+            setSeasonId(182);
+        }
+    }, [program]);
+
+    // Change url based upon query updates
+    useEffect(() => {
+        const url = `/rankings?program=${program}&seasonId=${seasonId}&region=${selectedRegion}`;
+        window.history.replaceState(null, '', url);
+    }, [program, seasonId, selectedRegion]);
   
-  const initialRender = useRef(true);
+    return (
+      <div>
+            <h1 className="title leftDisplay ml-4">Ratings</h1>
 
-  console.log(program + " " + seasonId + " " + loading);
+            {/* DropDowns*/}
+            <div className="dropdownDisplay" style={{left: "-23px" }}>
+                <ProgramDropdown setProgram={setProgram} program={program}  all = {false} />
+                <SeasonDropdown setSeasonId={setSeasonId} seasonId={seasonId} type={program} grade='' restrict = {null} />
+                <RegionDropdown onSelect={setSelectedRegion} value = {selectedRegion}/>
+            </div>
 
-  useEffect(() => {
-    // Skip the effect on the initial render
-    if (initialRender.current) {
-      initialRender.current = false;
-      return;
-    }
-
-    if (program === 'VRC') {
-      setSeasonId(181);
-    } else {
-      setSeasonId(182);
-    }
-  }, [program]);
-
-  useEffect(() => {
-    const url = `/rankings?program=${program}&seasonId=${seasonId}&region=${selectedRegion}`;
-    window.history.replaceState(null, '', url);
-  }, [seasonId, selectedRegion]);
-  
-  return (
-    <div>
-      <h1 className="title leftDisplay ml-4">Ratings</h1>
-      <div className="dropdownDisplay" style={{left: "-23px" }}>
-        <div >
-          <ProgramDropdown setProgram={setProgram} program={program}  all = {false} />
-        </div>
-        <div>
-          <SeasonDropdown setSeasonId={setSeasonId} seasonId={seasonId} type={program} grade='' restrict = {null} />
-        </div>
-        <div>
-          <RegionDropdown onSelect={setSelectedRegion} value = {selectedRegion}/>
-        </div>
+            <div className = "eventDisplay"> 
+                <RatingsList program={program} season={seasonId.toString()} region={selectedRegion} />
+            </div>
       </div>
-      <div className = "eventDisplay"> 
-        <SeasonRankings program={program} season={seasonId.toString()} region={selectedRegion} />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Rankings;
