0af6dce0bf396cbd1f5c825cd944f1aef7a36f68
Ray <rguo1010@uw.edu>
2024-04-14 21:13:11 -0700

    Lots of text changes + env

--- web-app/src/components/TeamInfo/TeamReveals.tsx
+++ web-app/src/components/TeamInfo/TeamReveals.tsx
@@ -1,59 +1,60 @@
 import React, { useState, useRef } from 'react';
 import { Box, Typography, IconButton } from '@mui/material';
 import ReactPlayer from 'react-player';
-import { ChevronLeft, ChevronRight } from '@mui/icons-material'; // Importing icons from Material-UI
+import { ChevronLeft, ChevronRight } from '@mui/icons-material';
 
+// Display reveal video
 interface Reveals {
-  post_date: string;
-  reveal_title: string;
-  reveal_url: string;
+    post_date: string;
+    reveal_title: string;
+    reveal_url: string;
 }
 
 interface TeamRevealsProps {
-  reveals: Reveals[];
+    reveals: Reveals[];
 }
 
 const TeamReveals: React.FC<TeamRevealsProps> = ({ reveals }) => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const playerRef = useRef<HTMLDivElement>(null);
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const playerRef = useRef<HTMLDivElement>(null);
 
-  const handleNextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide === reveals.length - 1 ? 0 : prevSlide + 1));
-    scrollToVideo();
-  };
+    const handleNextSlide = () => {
+        setCurrentSlide((prevSlide) => (prevSlide === reveals.length - 1 ? 0 : prevSlide + 1));
+        scrollToVideo();
+    };
 
-  const handlePreviousSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide === 0 ? reveals.length - 1 : prevSlide - 1));
-    scrollToVideo();
-  };
+    const handlePreviousSlide = () => {
+        setCurrentSlide((prevSlide) => (prevSlide === 0 ? reveals.length - 1 : prevSlide - 1));
+        scrollToVideo();
+    };
 
-  const scrollToVideo = () => {
-    if (playerRef.current) {
-      playerRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
-  };
+    const scrollToVideo = () => {
+        if (playerRef.current) {
+            playerRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+    };
 
-  return (
-    <Box maxWidth="md" mx="auto">
-      <Typography variant="h5" gutterBottom align="center">
-        {reveals[currentSlide].reveal_title}
-      </Typography>
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <IconButton onClick={handlePreviousSlide} style={{ color: 'black', marginRight: '8px' }}>
-          <ChevronLeft />
-        </IconButton>
-        <Box ref={playerRef} style={{ width: '100%', height: '400px', overflow: 'hidden' }}>
-          <ReactPlayer url={reveals[currentSlide].reveal_url} width="100%" height="100%" controls />
+    return (
+        <Box maxWidth="md" mx="auto">
+            <Typography variant="h5" gutterBottom align="center">
+                {reveals[currentSlide].reveal_title}
+            </Typography>
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <IconButton onClick={handlePreviousSlide} style={{ color: 'black', marginRight: '8px' }}>
+                    <ChevronLeft />
+                </IconButton>
+                <Box ref={playerRef} style={{ width: '100%', height: '400px', overflow: 'hidden' }}>
+                    <ReactPlayer url={reveals[currentSlide].reveal_url} width="100%" height="100%" controls />
+                </Box>
+                <IconButton onClick={handleNextSlide} style={{ color: 'black', marginLeft: '8px' }}>
+                    <ChevronRight />
+                </IconButton>
+            </Box>
+            <Typography variant="body1" align="center" mt={2}>
+                {`${currentSlide + 1} of ${reveals.length}`}
+            </Typography>
         </Box>
-        <IconButton onClick={handleNextSlide} style={{ color: 'black', marginLeft: '8px' }}>
-          <ChevronRight />
-        </IconButton>
-      </Box>
-      <Typography variant="body1" align="center" mt={2}>
-        {`${currentSlide + 1} of ${reveals.length}`}
-      </Typography>
-    </Box>
-  );
+    );
 };
 
 export default TeamReveals;
