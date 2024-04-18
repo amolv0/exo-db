174eca70efacb034903b626043a3243272168610
ghijj <amolgvaidya@outlook.com>
2024-04-18 00:21:12 -0400

    create audit_reveals to remove duplicate reveals, update updateReveals to properly check for duplicates of youtube url formats

--- database/audit_match_season.py
+++ database/audit_match_season.py
@@ -1,9 +1,32 @@
 import boto3
+import json
+from decimal import Decimal
 
 dynamodb = boto3.resource('dynamodb')
 event_table = dynamodb.Table('event-data')
 match_table = dynamodb.Table('match-data')
+s3_client = boto3.client('s3')
 
+def decimal_default(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)  # Convert Decimal to float
+    elif isinstance(obj, dict):
+        return {k: decimal_default(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [decimal_default(v) for v in obj]
+    else:
+        return obj  
+    
+def float_default(obj):
+    if isinstance(obj, float):
+        return Decimal(str(obj))  # Convert float to Decimal
+    elif isinstance(obj, dict):
+        return {k: float_default(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [float_default(v) for v in obj]
+    else:
+        return obj
+    
 def update_match_seasons_for_event(event_id):
     response = event_table.get_item(Key={'id': event_id})
     event = response.get('Item')
@@ -18,6 +41,12 @@ def process_event(event):
     season_id = event.get('season', {}).get('id', None)
     if season_id:
         divisions = event.get('divisions', [])
+        if 'divisions_s3_reference' in divisions:
+            bucket_name, key = divisions['divisions_s3_reference'].replace("s3://", "").split("/", 1)
+            response = s3_client.get_object(Bucket=bucket_name, Key=key)
+            divisions = json.loads(response['Body'].read().decode('utf-8'))
+            in_s3 = True
+            divisions = float_default(divisions)
         for division in divisions:
             matches = division.get('matches', [])
             for match in matches:
@@ -43,6 +72,12 @@ def process_events(events):
         season_id = event.get('season', {}).get('id', None)
         if season_id:
             divisions = event.get('divisions', [])
+            if 'divisions_s3_reference' in divisions:
+                bucket_name, key = divisions['divisions_s3_reference'].replace("s3://", "").split("/", 1)
+                response = s3_client.get_object(Bucket=bucket_name, Key=key)
+                divisions = json.loads(response['Body'].read().decode('utf-8'))
+                in_s3 = True
+                divisions = float_default(divisions)
             for division in divisions:
                 matches = division.get('matches', [])
                 for match in matches:
@@ -52,9 +87,8 @@ def process_events(events):
 def update_match_data(match_id, season_id):
     response = match_table.get_item(Key={'id': match_id})
     match_data = response.get('Item')
-    
-
     if not match_data or 'season' not in match_data:
+        print(f"Updating match {match_id}")
         match_table.update_item(
             Key={'id': match_id},
             UpdateExpression='SET season = :val',
@@ -64,4 +98,4 @@ def update_match_data(match_id, season_id):
         )
 
 if __name__ == "__main__":
-    update_match_seasons_for_event(54701)
+    update_match_seasons_for_event(49725)
