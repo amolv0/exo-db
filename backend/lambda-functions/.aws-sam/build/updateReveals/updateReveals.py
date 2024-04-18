ab5dc425243f583c694e206faa583b261a252c35
ghijj <amolgvaidya@outlook.com>
2024-04-18 00:21:12 -0400

    create audit_reveals to remove duplicate reveals, update updateReveals to properly check for duplicates of youtube url formats

--- backend/lambda-functions/.aws-sam/build/updateReveals/updateReveals.py
+++ backend/lambda-functions/.aws-sam/build/updateReveals/updateReveals.py
@@ -1,13 +1,22 @@
 import boto3
 import json
 import logging
+from urllib.parse import urlparse, parse_qs
 
-# Initialize the DynamoDB and SQS clients
 dynamodb = boto3.resource('dynamodb')
 sqs = boto3.client('sqs')
 
 logging = logging.getLogger()
 logging.setLevel("INFO")
+
+def normalize_youtube_url(url):
+    parsed_url = urlparse(url)
+    if 'youtu.be' in parsed_url.netloc:
+        return parsed_url.path.lstrip('/')
+    elif 'youtube.com' in parsed_url.netloc:
+        query_params = parse_qs(parsed_url.query)
+        return query_params['v'][0] if 'v' in query_params else None
+    return None
 
 def handler(event, context):
     table_name = 'team-data'
@@ -17,7 +26,7 @@ def handler(event, context):
         message_body = record['body']
         logging.info(f"Received message body: {message_body}")
         message = json.loads(message_body)
-
+        
         team_number = message['team_number']
 
         response = table.query(
@@ -36,16 +45,15 @@ def handler(event, context):
             team_item = max(response['Items'], key=lambda item: len(item.get('skills', [])))
             item_id = team_item['id']
 
-            # Check if 'reveals' exists and if the new reveal is already included
             current_reveals = team_item.get('reveals', [])
+            new_reveal_url = normalize_youtube_url(message['reveal_url'])
             new_reveal = {
-                'reveal_url': message['reveal_url'],
+                'reveal_url': new_reveal_url,
                 'reveal_title': message['reveal_title'],
                 'post_date': message['post_date']
             }
-            if not any(reveal['reveal_url'] == new_reveal['reveal_url'] for reveal in current_reveals):
+            if not any(normalize_youtube_url(reveal['reveal_url']) == new_reveal_url for reveal in current_reveals):
                 logging.info(f"Adding reveal {message['reveal_title']} to team {team_number}")
-                # Update the item in DynamoDB
                 update_expression = 'SET reveals = list_append(if_not_exists(reveals, :empty_list), :reveal_data)'
                 table.update_item(
                     Key={'id': item_id},
